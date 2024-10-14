5543ec4073beb5be5c0707bbdeaf260e5e4c08c7
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 14:28:49 +0900

    Title: 두 수의 합, Time: 159ms, Memory: 36MB, Status: Passed - CodeTree

--- 241014/두 수의 합/sum-of-two-num.py
+++ 241014/두 수의 합/sum-of-two-num.py
@@ -1,17 +1,21 @@
-n, k = map(int, input().split())
-li = list(map(int, input().split()))
-dic = {} # dict[i-k] += 1
-cnt = 0
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-for i in range(n):
-    a = k-li[i]
-    if a not in dic:
-        dic[a] = 0
-    dic[a] += 1
+count = dict()
 
-answer = 0
-for i in range(n):
-    if li[i] in dic:
-        answer += dic[li[i]]//2
+ans = 0
+# 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+for elem in arr:
+    diff = k - elem
+    # 가능한 모든 쌍의 수를 세어줍니다.
+    if diff in count:
+        ans += count[diff]
 
-print(answer)
+    # 현재 숫자의 개수를 하나 증가시켜줍니다.
+    if elem in count:
+        count[elem] += 1
+    else:
+        count[elem] = 1
+
+print(ans)
