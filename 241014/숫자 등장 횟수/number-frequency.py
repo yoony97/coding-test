cb3ea02d35d553f17ede7ad7dc70c24a506c22dd
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 13:50:57 +0900

    Title: 숫자 등장 횟수, Time: 801ms, Memory: 37MB, Status: Passed - CodeTree

--- 241014/숫자 등장 횟수/number-frequency.py
+++ 241014/숫자 등장 횟수/number-frequency.py
@@ -1,14 +1,19 @@
 n, m = map(int, input().split())
 li1 = list(map(int, input().split()))
 li2 = list(map(int, input().split()))
-maximum = max(max(li1),max(li2))
-num2count = { i : 0 for i in range(1, maximum+1)}
+
+num2count ={}
 
 for i, num in enumerate(li1):
+    if num not in num2count:
+        num2count[num] = 0
     num2count[num] += 1 
 
 s = ''
 for j in li2:
-    s += str(num2count[j]) +' '
+    if j not in num2count:
+        s += '0 '
+    else:
+        s += str(num2count[j]) +' '
 
 print(s.strip())
