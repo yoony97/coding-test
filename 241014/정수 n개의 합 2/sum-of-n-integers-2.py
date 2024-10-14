3c60a3bf5bd48ddcf6c3b5444cbd72292ee29551
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 16:23:54 +0900

    Title: 정수 n개의 합 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+li = list(map(int, input().split()))
+prefix = [0]*(n+1)
+
+for i in range(1, n):
+    prefix[i] = prefix[i-1] + li[i]
+
+a = 0
+maximum = 0 
+for i in range(k, n, k):
+    if maximum < prefix[i] - prefix[a]:
+        maximum = prefix[i] - prefix[a]
+    a = i
+print(maximum)
