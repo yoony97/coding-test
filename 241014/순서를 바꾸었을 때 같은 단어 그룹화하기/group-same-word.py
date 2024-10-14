b00cfa52f0ef0e105f28ae440a93f9d1b1c171ce
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 15:28:59 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 333ms, Memory: 25MB, Status: Passed - CodeTree

--- 241014/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 241014/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -5,14 +5,9 @@ d = {}
 
 for i in range(N):
     isinclude = False
-    string = input()
-    for j in permutations(string, len(string)):
-        key = ''.join(j)
-        if key in d:
-            d[key] += 1
-            isinclude = True
-            break
-    if not isinclude:
-        d[string] = 1
-
+    key = ''.join(sorted(input()))
+    if key in d:
+        d[key] += 1
+    else:
+        d[key] = 1
 print(max(d.values()))
