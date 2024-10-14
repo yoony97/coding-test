ca102af9d671a9905747f6d3bc87087d9e297aa6
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 16:42:51 +0900

    Title: 정수 n개의 합 3, Time: 186ms, Memory: 27MB, Status: Passed - CodeTree

--- 241014/정수 n개의 합 3/sum-of-n-integers-3.py
+++ 241014/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -4,15 +4,16 @@ for i in range(n):
     maps.append([0] + list(map(int, input().split())))
 
 prefix = [[0]*(n+1) for i in range(n+1)]
+
 for i in range(1,n+1):
     for j in range(1,n+1):
-        prefix[i][j] = prefix[i-1][j] + prefix[i][j] - prefix[i-1][j-1] + maps[i][j]
+        prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + maps[i][j]
 
 def get_sum(x1, y1, x2, y2):
     return prefix[x2][y2]     - prefix[x1 - 1][y2] - \
            prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]
 
-answer = 0 
+answer = -9999999999999
 for i in range(1, n-k +2):
     for j in range(1, n-k+2):
         answer = max(answer, get_sum(i, j, i+k -1 , j+k-1))
