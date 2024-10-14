908b0203859b934bb1148cda92c379fb5be18f4a
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-14 14:24:11 +0900

    Title: 두 수의 합, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/두 수의 합/sum-of-two-num.py
+++ 241014/두 수의 합/sum-of-two-num.py
@@ -1,10 +1,17 @@
 n, k = map(int, input().split())
 li = list(map(int, input().split()))
-
+dic = {} # dict[i-k] += 1
 cnt = 0
-for i in range(n):
-    for j in range(i+1,n):
-        if li[i] + li[j] == k:
-            cnt+=1
 
-print(cnt)
+for i in range(n):
+    a = k-li[i]
+    if a not in dic:
+        dic[a] = 0
+    dic[a] += 1
+
+answer = 0
+for i in range(n):
+    if li[i] in dic:
+        answer += dic[li[i]]
+
+print(answer//2)
