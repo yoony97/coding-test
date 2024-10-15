36213e172f0cb36f3e9a2f4798a1b5e045d1b0e3
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-15 12:26:35 +0900

    Title: 연속한 K개의 숫자, Time: 2033ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 241015/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 241015/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,31 +1,43 @@
 def get_sum(S, s,e):
     return S[e] - S[s-1]
 
+def count(numbers, s,e):
+    count = 0
+    for i in range(s, e-1):
+        if numbers[i] != 0:
+            count += 1
+    return count
+
+
 N, K, B = map(int, input().split())
+original = list(range(1, N+1))
 onumbers = list(range(1, N+1))
-bnumbers = list(range(1, N+1))
+bnumbers = [0]*N
 oS = [0]*(N+1)
 bS = [0]*(N+1)
+original_fix = [0]*(N+1)
 
-
-li = []
 for i in range(B):
     blank = int(input())
-    bnumbers[blank-1] = 0
-    li.append(blank)
+    bnumbers[blank-1] = blank
+    onumbers[blank-1] = 0
 
 for i in range(1, N+1):
     oS[i] = oS[i-1] + onumbers[i-1]
     bS[i] = bS[i-1] + bnumbers[i-1]
+    original_fix[i] = original_fix[i-1] + original[i-1]
 
 d = {}
+
+
+answer = 99999999999
+
 for i in range(1,N-K+1):
+    o = get_sum(original_fix, i, i+K-1)
     osum = get_sum(oS, i, i+K-1)
     bsum = get_sum(bS, i, i+K-1) 
-    diff = osum - bsum
-    if diff in li:
-        if diff in d:
-            d[diff] += 1
-        else:
-            d[diff] = 1
-print(d)
+    
+    if o == osum + bsum:
+        answer = min(answer, count(bnumbers, i, i+K-1))
+    
+print(answer)
