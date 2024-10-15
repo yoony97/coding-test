44041ef39828ef2282f2f40a671f7bb06a78bab0
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-15 14:45:41 +0900

    Title: 동전 더하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241015/동전 더하기/add-coins.py
@@ -0,0 +1,14 @@
+# 주어진 동전들이 전부 배수관계에 놓여있기 때문입니다
+# 배수 관계에 놓여있는 동전이 주어졌을 때에는, 항상 큰 동전부터 이용하는 것이 좋음을 알 수 있습니다.
+
+N, K = map(int, input().split())
+li = []
+for i in range(N):
+    li.append(int(input()))
+
+ans = 0
+for i in reversed(li):
+    K = K - K//i
+    ans += K//i
+
+print(ans)
