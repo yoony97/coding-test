f5c51a46d422f9f37ac486a339f778b6089e5156
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-15 14:55:51 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 129ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 241015/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 241015/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -1,13 +1,12 @@
 N = int(input())
 li = list(map(int, input().split()))
 
-start = 0
-end = 0
-sums = sum(li[start:end])
-for i in range(1,N):
-    if sums + li[i] > li[i]:
-        end = i
-    else:
-        start = i
 
-print(sum(li[start:end]))
+s = li[0]
+
+for i in range(1,N):
+    if s + li[i] > li[i]:
+        s += li[i]
+    else:
+        s = li[i]
+print(s)
