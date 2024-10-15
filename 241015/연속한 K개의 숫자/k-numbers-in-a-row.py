4db6d73fc2141fa5dc4a8330b06afd2338492912
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-15 12:46:03 +0900

    Title: 연속한 K개의 숫자, Time: 5087ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 241015/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 241015/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,12 +1,13 @@
 def get_sum(S, s,e):
     return S[e] - S[s-1]
 
-def count(numbers, s,e):
-    count = 0
-    for i in range(s-1, e):
-        if numbers[i] != 0:
-            count += 1
-    return count
+def preprocess(numbers):
+    n = len(numbers)
+    prefix = [0] * (n + 1)  # prefix[i]는 numbers[0]부터 numbers[i-1]까지의 0이 아닌 수 개수
+    for i in range(n):
+        prefix[i + 1] = prefix[i] + (1 if numbers[i] != 0 else 0)
+    return prefix
+
 
 
 N, K, B = map(int, input().split())
@@ -25,6 +26,7 @@ for i in range(B):
 for i in range(1, N+1):
     oS[i] = oS[i-1] + onumbers[i-1]
     bS[i] = bS[i-1] + bnumbers[i-1]
+    counting = preprocess(bnumbers)
     original_fix[i] = original_fix[i-1] + original[i-1]
 
 d = {}
@@ -38,6 +40,6 @@ for i in range(1,N-K+1):
     bsum = get_sum(bS, i, i+K-1) 
     
     if o == osum + bsum:
-        answer = min(answer, count(bnumbers, i, i+K-1))
+        answer = min(answer, get_sum(counting, i, i+K-1))
     
 print(answer)
