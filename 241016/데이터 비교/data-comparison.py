c76f92997105a6feff091959392114281c1800a1
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-16 15:34:12 +0900

    Title: 데이터 비교, Time: 195ms, Memory: 37MB, Status: Passed - CodeTree

--- 241016/데이터 비교/data-comparison.py
+++ 241016/데이터 비교/data-comparison.py
@@ -1,13 +1,13 @@
 n = int(input())
 arr1 = set(map(int, input().split()))
 m = int(input())
-arr2 = set(map(int, input().split()))
+arr2 = list(map(int, input().split()))
 
 s = []
-for i in range(m):
-    if arr2[i] in arr1:
+for elem in arr2:
+    if elem in arr1:
         s.append('1')
     else:
-        s.append('0')
+        s.append('0')    
 
 print(' '.join(s))
