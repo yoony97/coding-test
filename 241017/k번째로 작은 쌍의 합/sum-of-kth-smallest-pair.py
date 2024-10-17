23b9eb2b62bc0106fbfd35fec6e340de3974dc87
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-17 14:14:37 +0900

    Title: k번째로 작은 쌍의 합, Time: 286ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 241017/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 241017/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -4,12 +4,23 @@ import sys
 data = sys.stdin.read().strip().split("\n")
 
 N, M, K = map(int, data[0].split())
+A = list(map(int, data[1].split()))
+B = list(map(int, data[2].split()))
+
 q = []
-for i in list(map(int, data[1].split())):
-    for j in list(map(int, data[2].split())):
-        heapq.heappush(q, (i+j, i,j))
 
-for i in range(K):
-    s, x, y = heapq.heappop(q)
+for i in range(N):
+    for j in range(M):
+        total = A[i] + B[j]
 
-print(s)
+        if len(q) < K:
+            heapq.heappush(q, (-total, A[i], B[j]))
+        else:
+            if -q[0][0] > total:
+                heapq.heapreplace(q, (-total, A[i], B[j]))
+            else:
+                break  # B는 정렬되어 있으므로 더 볼 필요 없음
+
+# 최종적으로 K번째 작은 합을 출력
+result = -heapq.heappop(q)[0]
+print(result)
