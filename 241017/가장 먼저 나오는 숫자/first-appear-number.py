841c077f6160406b7710350c58f06c02641d78fd
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-17 16:35:00 +0900

    Title: 가장 먼저 나오는 숫자, Time: 260ms, Memory: 34MB, Status: Passed - CodeTree

--- 241017/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241017/가장 먼저 나오는 숫자/first-appear-number.py
@@ -11,6 +11,23 @@ def lower_bound(target, arr, n):
             left = mid + 1
     return min_idx
 
+def check(target, arr, n):
+    left = 0
+    right = n-1
+    min_idx = n
+    while left <= right:
+        mid = (left+right)//2
+        if arr1[mid] > target:
+            #min_idx = min(min_idx, mid)
+            right = mid - 1
+        elif arr1[mid] < target:
+            left = mid + 1
+        elif arr1[mid] == target:
+            return True
+
+    return False
+
+
 
 
 N, M = map(int, input().split())
@@ -18,8 +35,7 @@ arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 
 for i in arr2:
-    answer = lower_bound(i, arr1, N)
-    if answer == N:
-        print(-1)
+    if check(i, arr1, N):
+        print(lower_bound(i, arr1, N)+1)
     else:
-        print(answer+1)
+        print(-1)
