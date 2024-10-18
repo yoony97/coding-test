8defafa53846437a87aab601d19c2dc6f40d7677
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-18 13:07:58 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
+++ 241018/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -1,5 +1,4 @@
 import math
-
 m = int(input())
 a, b = map(int, input().split())
 
@@ -7,27 +6,24 @@ def explore(target):
     cnt = 0
     left = 0
     right = m
-
     while left <= right:
-        mid = (left + right+1) // 2  # 정수 나눗셈으로 mid 계산
+        mid = (left + right+1)//2
         cnt += 1
 
         if mid > target:
-            right = mid - 1
-        elif mid < target:
-            left = mid + 1
+            right = mid-1
+        elif mid == target:
+            return cnt
         else:
-            return cnt  # 찾으면 탐색 횟수 반환
-
-    return -1  # (실제로 이 경우는 발생하지 않음)
+            left = mid+1
+        
+    return -1
 
 minimum = float('inf')
 maximum = 0
-
-# 모든 i에 대해 이진 탐색 수행
-for i in range(a, b + 1):
+for i in range(a, b+1):
     cnt = explore(i)
-    minimum = min(minimum, cnt)
     maximum = max(maximum, cnt)
+    minimum = min(minimum, cnt)
 
-print(minimum, maximum)
+print(minimum,maximum)
