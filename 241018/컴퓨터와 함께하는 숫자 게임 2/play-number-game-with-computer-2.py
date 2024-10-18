ba8b4c938dae0b9b69e61b94bd9f14d77df00d66
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-18 12:55:08 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 5093ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 241018/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.py
@@ -0,0 +1,29 @@
+import math
+m = int(input())
+a, b = map(int, input().split())
+
+def explore(target):
+    cnt = 0
+    left = 0
+    right = m
+    while left <= right:
+        mid = int(math.ceil((left + right)/2))
+        cnt += 1
+
+        if mid > target:
+            right = mid-1
+        elif mid == target:
+            return cnt
+        else:
+            left = mid+1
+        
+    return -1
+
+minimum = float('inf')
+maximum = 0
+for i in range(a, b+1):
+    cnt = explore(i)
+    maximum = max(maximum, cnt)
+    minimum = min(minimum, cnt)
+
+print(minimum,maximum)
