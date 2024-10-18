786e8870aff12d0686f0ab881d663da0577e3439
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-10-18 16:13:43 +0900

    Title: 삼 오 무, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/삼 오 무/three-five-moo.py
+++ 241018/삼 오 무/three-five-moo.py
@@ -2,18 +2,19 @@ n = int(input())
 
 left = 1
 right = 10**9
+
 def get_count(target):
     return target - (target // 3 + target // 5 - target // 15)
 
-
+answer = -1
 
 while left <= right:
-    mid = (left+right)//2
-    #print(left, right, mid)
-    if get_count(mid) > n:
-        right = mid - 1
-    elif get_count(mid) < n:
-        left = mid + 1
+    mid = (left + right) // 2
+    if get_count(mid) >= n:  # n 이상인 경우를 찾음
+        answer = mid  # 잠정적으로 저장
+        right = mid - 1  # 더 작은 값이 있는지 탐색
     else:
-        print(mid)
-        break
+        left = mid + 1
+
+# 출력: 정확한 N번째 값 출력
+print(answer)
