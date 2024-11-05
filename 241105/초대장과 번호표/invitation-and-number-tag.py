f7941ff04d05c3888afbad32b60164454fb622b4
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-05 13:33:23 +0900

    Title: 초대장과 번호표, Time: 5062ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 241105/초대장과 번호표/invitation-and-number-tag.py
+++ 241105/초대장과 번호표/invitation-and-number-tag.py
@@ -7,7 +7,7 @@ groups = [list(map(int, i.split()[1:])) for i in data]
 groups.sort(key = lambda x: (len(x), x[0]))
 #print(groups)
 s = set([groups[0][0]])
-for _ in range(100):
+for _ in range(G):
     for i in groups:
         if len(set(i) - s) < 2:
             s = s.union(set(i))    
