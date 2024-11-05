0d76542b7bde7ff91d80bffdf90c101067b9d9f6
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-05 13:30:53 +0900

    Title: 초대장과 번호표, Time: 55ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241105/초대장과 번호표/invitation-and-number-tag.py
+++ 241105/초대장과 번호표/invitation-and-number-tag.py
@@ -4,11 +4,12 @@ data = sys.stdin.read().strip().split("\n")
 N, G  = map(int, data[0].split())
 data = data[1:]
 groups = [list(map(int, i.split()[1:])) for i in data]
-groups.sort(key = lambda x: len(x))
-
+groups.sort(key = lambda x: (len(x), x[0]))
+#print(groups)
 s = set([groups[0][0]])
-for i in groups:
-    if len(set(i) - s) < 2:
-        s = s.union(set(i))    
-
+for _ in range(3):
+    for i in groups:
+        if len(set(i) - s) < 2:
+            s = s.union(set(i))    
+#        print(s)
 print(len(s))
