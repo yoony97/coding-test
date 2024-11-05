bcdf3751fae6c7ed1b84764f3d2a43f6ddb54560
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-05 14:09:21 +0900

    Title: 초대장과 번호표, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241105/초대장과 번호표/invitation-and-number-tag.py
+++ 241105/초대장과 번호표/invitation-and-number-tag.py
@@ -1,15 +1,25 @@
 import sys
-
+from collections import deque
 data = sys.stdin.read().strip().split("\n")
 N, G  = map(int, data[0].split())
 data = data[1:]
 groups = [list(map(int, i.split()[1:])) for i in data]
 groups.sort(key = lambda x: (len(x), x[0]))
-#print(groups)
+queue = deque(groups)
 s = set([groups[0][0]])
-for _ in range(G//2):
-    for i in groups:
-        if len(set(i) - s) < 2:
-            s = s.union(set(i))    
-#        print(s)
+pre_len = len(queue)
+while queue:
+    i = queue.popleft()
+    
+    if len(set(i) - s) < 2:
+        s = s.union(set(i))    
+    
+    else:
+        queue.append(i)
+
+    if pre_len == len(queue):
+        break
+    
+    pre_len = len(queue)
+
 print(len(s))
