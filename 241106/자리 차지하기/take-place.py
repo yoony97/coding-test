042250bf1942bbf086b2124212c179e965ceaf16
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-06 18:21:19 +0900

    Title: 자리 차지하기, Time: 91ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 241106/자리 차지하기/take-place.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedSet
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+s = SortedSet([i for i in range(1, m+1)]) #i번째 사람은 1번 이상 a_i번 이하의 의자에만 앉고 싶다
+cnt = 0
+for i in li: 
+    print(s)
+    idx = s.bisect_left(i)
+    if idx == 0 and s[idx] != i:
+        print(cnt)
+        break
+    else:
+        s.remove(s[idx])
+        cnt += 1
