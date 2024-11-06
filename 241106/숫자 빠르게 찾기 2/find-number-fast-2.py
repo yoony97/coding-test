15e028ba29230c037021c4c035e706cc9bf7fca7
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-06 15:39:59 +0900

    Title: 숫자 빠르게 찾기 2, Time: 516ms, Memory: 37MB, Status: Passed - CodeTree

--- 241106/숫자 빠르게 찾기 2/find-number-fast-2.py
+++ 241106/숫자 빠르게 찾기 2/find-number-fast-2.py
@@ -1,4 +1,4 @@
-frorm sortedcontainers import SortedSet
+from sortedcontainers import SortedSet
 import sys
 
 
@@ -7,7 +7,7 @@ n, m = map(int, data[0].split())
 li = list(map(int, data[1].split()))
 s = SortedSet(li)
 for i in data[2:]:
-    idx = s.bisect_left(int(i)
+    idx = s.bisect_left(int(i))
     if idx < len(s):
         print(s[idx])
     else:
