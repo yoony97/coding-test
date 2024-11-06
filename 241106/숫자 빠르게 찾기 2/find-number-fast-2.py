92ce924e3e4bba19867dbbf7df91dc459d605eed
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-06 15:38:45 +0900

    Title: 숫자 빠르게 찾기 2, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241106/숫자 빠르게 찾기 2/find-number-fast-2.py
@@ -0,0 +1,14 @@
+frorm sortedcontainers import SortedSet
+import sys
+
+
+data = sys.stdin.read().strip().split("\n")
+n, m = map(int, data[0].split())
+li = list(map(int, data[1].split()))
+s = SortedSet(li)
+for i in data[2:]:
+    idx = s.bisect_left(int(i)
+    if idx < len(s):
+        print(s[idx])
+    else:
+        print(-1)
