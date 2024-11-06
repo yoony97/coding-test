e86bea4b9aab58135c44711f584ed7dc1344ff6e
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-06 18:06:51 +0900

    Title: 가까운 숫자, Time: 1289ms, Memory: 34MB, Status: Wrong Answer - CodeTree

---
+++ 241106/가까운 숫자/nearest-number.py
@@ -0,0 +1,14 @@
+from sortedcontainers import SortedSet
+
+s = SortedSet([0])
+n = input()
+li = list(map(int, input().split()))
+minimum = 999999999
+for i in li:
+    s.add(i)
+    idx = s.bisect_left(i)
+    if idx + 1 < len(s):
+        minimum = min(s[idx] - s[idx-1], s[idx+1] - s[idx])
+    else:
+        minimum = min(minimum, s[idx] - s[idx-1])
+    print(minimum)
