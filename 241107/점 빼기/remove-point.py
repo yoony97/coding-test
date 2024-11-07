f4e4e284a3ae12305360d3a512b381219b08eb16
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-07 15:05:56 +0900

    Title: 점 빼기, Time: 101ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 241107/점 빼기/remove-point.py
@@ -0,0 +1,19 @@
+# 각 질의에 대해 주어진 숫자 k보다 x값이 같거나 큰 점 중 x값이 가장 작은 점을 찾아 지우려고 합니다.
+# 만약 x값이 가장 작은 점이 여러 개라면, 그 중 y값이 가장 작은 점을 지우면 됩니다.
+import sys
+from sortedcontainers import SortedSet
+inputs = sys.stdin.read().strip().split("\n")
+
+s = SortedSet() #(x,y)
+n ,m = map(int, inputs[0].split())
+
+#init s
+for i in range(n):
+    s.add(tuple(map(int, inputs[i+1].split())))
+inputs = inputs[n+1:]
+
+#query
+for j in range(m):
+    idx = s.bisect_left(int(inputs[j]))
+    if idx < len(s):
+        print(s[idx])
