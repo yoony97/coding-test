332711229b11243fcc27f0840539089f2e8f50eb
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-08 14:39:41 +0900

    Title: 최대로 연속한 숫자, Time: 95ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241108/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 241108/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -6,29 +6,20 @@ n, m = map(int, input().split())
 s = SortedSet([i for i in range(n+1)])
 li = list(map(int, input().split()))
 
-#제거하는 함수
 def cal():
-    target = 1
-    cnt = 0
     max_len = 0
-    li = [s[0]]
-    offset = 0
-    for i in range(s[0]+1, s[-1]+1):
-        i = i + offset
-        if i== li[-1]+1 and i in s:
-            li.append(i)
+    current_len = 1
+
+    for i in range(1, len(s)):
+        # 연속된 숫자인지 확인
+        if s[i] == s[i-1] + 1:
+            current_len += 1
         else:
-            max_len = max(len(li), max_len)
-            idx = s.bisect_right(i)
-            offset += 1
-            if len(s) != idx:
-                li = [s[idx]]
-            else:
-                return max_len
+            max_len = max(max_len, current_len)
+            current_len = 1
 
+    max_len = max(max_len, current_len)
     return max_len
-
-
 
 
 for num in li:
