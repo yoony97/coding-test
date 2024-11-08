99ae519933841545905906549d77bbf17bcb30a4
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-08 13:10:19 +0900

    Title: 문제 추천 시스템 1, Time: 265ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241108/문제 추천 시스템 1/problem-recommendation-system-1.py
+++ 241108/문제 추천 시스템 1/problem-recommendation-system-1.py
@@ -1,10 +1,16 @@
 import sys
 from sortedcontainers import SortedSet
 s = SortedSet()
+#s = SortedSet()
+dic = dict()
 data = sys.stdin.read().strip().split("\n")
 n = int(data[0])
 for i in range(n):
     p, l = map(int, data[i+1].split())
+    if l in dic.keys():
+        dic[l].add(p)
+    else: 
+        dic[l] = set([p])
     s.add((l,p))
 
 k = int(data[n+1])
@@ -13,19 +19,24 @@ inputs_op = data[n+2:]
 for i in range(k):
     ops = inputs_op[i].split()
     if ops[0] == 'ad':
-        s.add((int(ops[2]), int(ops[1])))
-    
+        p, l = int(ops[1]), int(ops[2])
+        if l in dic: 
+            dic[l].add(p)
+        else:
+            dic[l] = set([p])
+        s.add((l, p))
+        
     if ops[0] == 'sv':
-        s.remove((int(ops[2]), int(ops[1])))
+        p, l = int(ops[1]), int(ops[2])
+        dic[l].remove(p)
+        s.remove((l, p))
     
     if ops[0] == 'rc':
         #가장 낮은 번호, 문제 번호 작은거
         if int(ops[1]) == -1:
             tl = s[0][0]
-            idx = s.bisect_left((tl, 0))
-            print(s[idx][1])
+            print(max(dic[tl]))
         #가장 높은 번호, 문제 번호 작은거
         if int(ops[1]) == 1:
             tl = s[-1][0]
-            idx = s.bisect_left((tl, 0))
-            print(s[idx][1])
+            print(max(dic[tl]))
