ea6fb03f0488f7979f7e29e9c270b3ec8715ea4a
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-08 14:41:06 +0900

    Title: 최대로 연속한 숫자, Time: 101ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241108/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 241108/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -1,16 +1,17 @@
 import sys
 from sortedcontainers import SortedSet
 
-
 n, m = map(int, input().split())
 s = SortedSet([i for i in range(n+1)])
 li = list(map(int, input().split()))
 
+# 제거 후 가장 긴 연속된 숫자들의 길이를 구하는 함수
 def cal():
-    max_len = 0
-    current_len = 1
     if not s:  # SortedSet이 비어 있는 경우
         return 0
+    
+    max_len = 0
+    current_len = 1
 
     for i in range(1, len(s)):
         # 연속된 숫자인지 확인
@@ -20,14 +21,10 @@ def cal():
             max_len = max(max_len, current_len)
             current_len = 1
 
-    max_len = max(max_len, current_len)
+    max_len = max(max_len, current_len)  # 마지막 연속 길이 체크
     return max_len
 
-
+# li에 있는 숫자들을 순서대로 제거하고, 연속된 숫자의 최대 길이를 출력
 for num in li:
     s.remove(num)
-    
     print(cal())
-#이게 왜 TreeSet인가?
-
-#계산하는 함수
