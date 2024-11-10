36e73d06ca06871f63bd70ad9b1827e852c58201
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 15:13:04 +0900

    Title: 중앙값, Time: 309ms, Memory: 28MB, Status: Passed - CodeTree

--- 241110/중앙값/median.py
+++ 241110/중앙값/median.py
@@ -1,16 +1,31 @@
-from sortedcontainers import SortedList
+import heapq
+#import sys
+#input = sys.stdin.read
 
 T = int(input())
 
-def solve(N:int, li:list):
+def solve(N: int, li: list):
+    left = []  # 최대 힙 (중앙값 이하를 저장)
+    right = []  # 최소 힙 (중앙값 이상을 저장)
+    
     for i in range(N):
-        if i%2 == 0:
-            temp = SortedList(li[:i+1])
-            #print(i, temp, i//2, temp[i//2])
-            print(temp[i//2], end=' ')
+        num = li[i]
         
-    
-    
+        # 최대 힙에 삽입
+        if not left or num <= -left[0]:
+            heapq.heappush(left, -num)
+        else:
+            heapq.heappush(right, num)
+        
+        # 균형 맞추기
+        if len(left) > len(right) + 1:
+            heapq.heappush(right, -heapq.heappop(left))
+        elif len(right) > len(left):
+            heapq.heappush(left, -heapq.heappop(right))
+        
+        # i가 짝수일 때마다 중앙값 출력
+        if i % 2 == 0:
+            print(-left[0], end=' ')
 
 for _ in range(T):
     N = int(input())
