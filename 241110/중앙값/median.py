b9022ec4a4b867d44555454415a125ea09a064da
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 15:07:41 +0900

    Title: 중앙값, Time: 87ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 241110/중앙값/median.py
@@ -0,0 +1,18 @@
+from sortedcontainers import SortedList
+
+T = int(input())
+
+def solve(N:int, li:list):
+    for i in range(N):
+        if i%2 == 0:
+            temp = SortedList(li[:i+1])
+            #print(i, temp, i//2)
+            print(temp[i//2-1])
+        
+    
+    
+
+for _ in range(T):
+    N = int(input())
+    li = list(map(int, input().split()))
+    solve(N, li)
