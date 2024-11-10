55b1fdb9dfb6b6c3b260c89ca518bfd6d971ecfe
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 14:47:53 +0900

    Title: 앞에서부터 삭제하기 2, Time: 89ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 241110/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241110/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -2,19 +2,42 @@ import heapq
 from sortedcontainers import SortedList
 N = int(input())
 li = list(map(int, input().split()))
-h = []
+idx_h = []
+value_h = []
+removed_items = set()
 answer = 0 
 
-def get_li(h):
-    return SortedList([i[1] for i in h])
 
 for idx, i in enumerate(li):
-    heapq.heappush(h, (idx, i))
+    heapq.heappush(idx_h, (idx, i))
+    heapq.heappush(value_h, (i, idx))
 
-while h:
-    heapq.heappop(h)
-    temp = get_li(h)[1:]
-    if len(temp) > 0:
-        answer = max(answer, sum(temp) / len(temp))
-        
+def pop_by_value():
+    while value_h:
+        value, idx = heapq.heappop(value_h)
+        if (idx, value) not in removed_items:
+            return idx, value
+
+def pop_by_idx():
+    while idx_h:
+        idx, value = heapq.heappop(idx_h)
+        if (idx, value) not in removed_items:
+            removed_items.add((idx,value))
+            return idx, value
+
+while len(idx_h) > 1:
+    total = 0
+    cnt = 0
+    _, _ = pop_by_idx() #원소 제거
+    idx, value = pop_by_value() #최솟값 제거
+    for v, i in value_h:
+        if (i, v) not in removed_items:
+            print(v, end=' ')
+            total += v
+            cnt += 1
+    print("")
+    heapq.heappush(value_h,(value, idx))
+    if cnt != 0:
+        answer = max(answer, total/cnt)
+
 print('%.2f' %answer)
