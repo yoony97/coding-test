fbac6b5f7806bc4fcfae12f473704da3003214ec
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 17:47:36 +0900

    Title: 최대로 연속한 숫자, Time: 903ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 241110/최대로 연속한 숫자/maximum-consecutive-number.py
+++ 241110/최대로 연속한 숫자/maximum-consecutive-number.py
@@ -1,16 +1,17 @@
+from sortedcontainers import SortedSet
+
 def max_consecutive_length(n, m, removals):
-    nums = set(range(n + 1))
+    nums = SortedSet(range(n + 1))
     results = []
 
     for removal in removals:
         nums.remove(removal)
 
-        sorted_nums = sorted(nums)
         max_length = 0
         current_length = 1
 
-        for i in range(1, len(sorted_nums)):
-            if sorted_nums[i] == sorted_nums[i - 1] + 1:
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
                 current_length += 1
             else:
                 max_length = max(max_length, current_length)
