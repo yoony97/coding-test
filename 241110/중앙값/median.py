b51ddc4df94ec90c6b2e64db34ce60011741bc50
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 15:08:02 +0900

    Title: 중앙값, Time: 90ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 241110/중앙값/median.py
+++ 241110/중앙값/median.py
@@ -7,7 +7,7 @@ def solve(N:int, li:list):
         if i%2 == 0:
             temp = SortedList(li[:i+1])
             #print(i, temp, i//2)
-            print(temp[i//2-1], end='')
+            print(temp[i//2-1], end=' ')
         
     
     
