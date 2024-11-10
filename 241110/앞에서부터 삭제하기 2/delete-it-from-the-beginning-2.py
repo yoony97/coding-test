985efc64a6e854e12c8316bf8fd8b7dd164f1c2f
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 15:00:17 +0900

    Title: 앞에서부터 삭제하기 2, Time: 97ms, Memory: 27MB, Status: Passed - CodeTree

--- 241110/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 241110/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -1,23 +1,32 @@
-from sortedcontainers import SortedList
+import heapq
 
-N = int(input())
-li = list(map(int, input().split()))
+# 입력:
+n = int(input())
+arr = list(map(int, input().split()))
 
-max_average = float('-inf')  # 최대 평균값 초기화
+# 변수 선언
+sum_val = 0
+max_avg = 0
+pq = []
 
-# K를 1부터 N-2까지 순회
-for K in range(1, N - 1):
-    # SortedList로 앞에서 K개 제거한 나머지 리스트 관리
-    remaining_list = SortedList(li[K:])  # 앞에서 K개를 제거한 리스트
-    
-    # 최솟값을 제거하고 평균 계산
-    min_value = remaining_list.pop(0)  # 최솟값 제거
-    total = sum(remaining_list)
-    count = len(remaining_list)
-    
-    # 평균 계산 및 최대 평균값 갱신
-    average = total / count
-    max_average = max(max_average, average)
+heapq.heappush(pq, arr[n - 1])
+sum_val += arr[n - 1]
+# k가 N - 2일 때부터 1일 때까지 거꾸로 탐색합니다.
+# priority queue를 이용하여 진행합니다.
+for i in range(n - 2, 0, -1):
+    # 앞에서부터 K개를 삭제하고 나면
+    # 뒤에 i ~ n - 1 까지의 숫자만이 남습니다.
+    heapq.heappush(pq, arr[i])
+    sum_val += arr[i]
 
-# 결과 출력
-print(f"{max_average:.2f}")
+    # 남아있는 정수 중 가장 작은 숫자를 찾아
+    # 그 숫자를 제외한 평균을 구합니다.
+    min_num = pq[0]
+    avg = (sum_val - min_num) / (n - i - 1)
+
+    # 평균이 최대가 된다면 정답을 현재 평균으로 갱신해줍니다.
+    if max_avg < avg:
+        max_avg = avg
+
+# 평균값의 최대를 출력합니다.
+print(f"{max_avg:.2f}")
