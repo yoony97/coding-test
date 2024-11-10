ee9ad3cf666ee0c8dcfaa251c5093ae1ddaa8a7a
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-10 15:09:02 +0900

    Title: 중앙값, Time: 5074ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 241110/중앙값/median.py
+++ 241110/중앙값/median.py
@@ -6,8 +6,8 @@ def solve(N:int, li:list):
     for i in range(N):
         if i%2 == 0:
             temp = SortedList(li[:i+1])
-            #print(i, temp, i//2)
-            print(temp[i//2-1], end=' ')
+            #print(i, temp, i//2, temp[i//2])
+            print(temp[i//2], end=' ')
         
     
     
