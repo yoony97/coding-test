0e4f3ea82c4e2f979d9107d089b55fa3340a6843
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 18:26:31 +0900

    Title: 자연수 M/2개의 쌍, Time: 59ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241113/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 241113/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -7,18 +7,19 @@ inputs = sys.stdin.read().strip().split("\n")
 n = int(inputs[0])
 numbers = []
 
+#여기서 메모리 에러가 발생해
 for i in range(n):
     x, y= map(int, inputs[i+1].split())
-    numbers.extend([y]*x)
+    numbers.append((x,y))
 
-numbers.sort()
+numbers.sort(key = lambda x: x[1])
+print(numbers[0][1] + numbers[-1][1])
+# offset = len(numbers)-1
+# min_value = float('inf')
 
-offset = len(numbers)-1
-min_value = float('inf')
+# for i in range(len(numbers)//2):
+#     #print(numbers[i],numbers[offset])
+#     min_value = min(min_value, numbers[i] + numbers[offset])
+#     offset = offset -1
 
-for i in range(len(numbers)//2):
-    #print(numbers[i],numbers[offset])
-    min_value = min(min_value, numbers[i] + numbers[offset])
-    offset = offset -1
-
-print(min_value)
+# print(min_value)
