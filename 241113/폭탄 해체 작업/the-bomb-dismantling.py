c34ea964ae2e86bd74fd58138a64b6c8054d98cc
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 17:30:33 +0900

    Title: 폭탄 해체 작업, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241113/폭탄 해체 작업/the-bomb-dismantling.py
+++ 241113/폭탄 해체 작업/the-bomb-dismantling.py
@@ -6,6 +6,7 @@ li = [tuple(map(int, i.split())) for i in inputs[1:]]
 times = set()
 
 li.sort(key= lambda x: x[0], reverse=True)
+print(li)
 answer = 0
 for (score, time) in li:
     if time not in times:
