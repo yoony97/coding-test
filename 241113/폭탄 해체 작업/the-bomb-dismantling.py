dd9c266c1520315c1259ede8e54c10deb1713b7f
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 17:30:11 +0900

    Title: 폭탄 해체 작업, Time: 39ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/폭탄 해체 작업/the-bomb-dismantling.py
@@ -0,0 +1,15 @@
+import sys
+
+inputs = sys.stdin.read().strip().split("\n")
+n = int(inputs[0])
+li = [tuple(map(int, i.split())) for i in inputs[1:]]
+times = set()
+
+li.sort(key= lambda x: x[0], reverse=True)
+answer = 0
+for (score, time) in li:
+    if time not in times:
+        times.add(time)
+        answer += score
+
+print(answer)
