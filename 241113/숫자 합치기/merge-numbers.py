8195346037a879156904185daaa0b2dfbbd6eb5a
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 16:06:18 +0900

    Title: 숫자 합치기, Time: 1633ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 241113/숫자 합치기/merge-numbers.py
+++ 241113/숫자 합치기/merge-numbers.py
@@ -3,13 +3,11 @@ li = list(map(int, input().split()))
 li.sort()
 
 answer = 0
-while li:
+while len(li) > 1:
     if len(li) > 1:
         a = li.pop(0)
         b = li.pop(0)
         li.append(a+b)
         answer += a+b
-    else:
-        answer += li.pop(0)
-
+    
 print(answer)
