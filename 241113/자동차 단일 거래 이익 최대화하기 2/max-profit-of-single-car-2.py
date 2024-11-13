6e5c2dfcbbf426b622c602db271b66e94d6b7966
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 17:15:22 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -0,0 +1,11 @@
+n = int(input())
+li = list(map(int, input().split()))
+buy = li[0]
+answer = 0
+for i in range(1, n):
+    if buy > li[i]:
+        buy = li[i]
+    print(buy, li[i])
+    answer = max(li[i]- buy, answer)
+    
+print(answer)
