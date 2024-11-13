a407e9ee52c91ed0b4e7b3ed5fc4b99b63882980
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 18:01:52 +0900

    Title: 폭탄 해체 작업, Time: 133ms, Memory: 21MB, Status: Passed - CodeTree

--- 241113/폭탄 해체 작업/the-bomb-dismantling.py
+++ 241113/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,16 +1,26 @@
-import sys
+import heapq
 
-inputs = sys.stdin.read().strip().split("\n")
-n = int(inputs[0])
-li = [tuple(map(int, i.split())) for i in inputs[1:]]
-times = set()
+# 입력
+N = int(input().strip())
+bombs = [tuple(map(int, input().split())) for _ in range(N)]
 
-li.sort(key= lambda x: (x[1], x[0]), reverse=True)
-answer = 0
-for (score, time) in li:
-    if time not in times:
-        times.add(time)
-        answer += score
+# 시간 제한 기준으로 오름차순 정렬
+bombs.sort(key=lambda x: x[1])
 
-print(answer)
+# 최대 점수 저장 변수 및 최대 힙 초기화
+total_score = 0
+max_heap = []
 
+# 폭탄을 시간 제한 기준으로 순차적으로 처리
+for score, limit_time in bombs:
+    # 최대 힙에 점수 추가
+    heapq.heappush(max_heap, score)
+    
+    # 해체 가능한 시간 초과 시 가장 점수가 낮은 폭탄 해제
+    if len(max_heap) > limit_time:
+        heapq.heappop(max_heap)
+
+# 최대 힙에 남아있는 점수 합산
+total_score = sum(max_heap)
+
+print(total_score)
