6b8be59adcfc85256cd767a85035a4221004bb20
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 16:17:03 +0900

    Title: 숫자 합치기, Time: 305ms, Memory: 27MB, Status: Passed - CodeTree

--- 241113/숫자 합치기/merge-numbers.py
+++ 241113/숫자 합치기/merge-numbers.py
@@ -1,17 +1,17 @@
+from sortedcontainers import SortedList
+
 n = int(input())
-li = list(map(int, input().split()))
-li.sort()
-#두 개 뽑아서
+li = SortedList(list(map(int, input().split())))
+
 answer = 0
 while li:
     if len(li) > 2:
         a = li.pop(0)
         b = li.pop(0)
-        li.append(a+b)
+        li.add(a+b)
         answer += (a+b)
     else:
         answer += li[0] + li[1]
         print(answer)
         break
     
-#print(answer)
