cd362feaaa3c5f7ec3d883e9e3c9d3d48da93521
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-13 17:22:44 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241113/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -0,0 +1,32 @@
+import sys
+
+inputs = sys.stdin.read().strip().split("\n")
+n = int(inputs[0])
+cards = set([i for i in range(1,2*n+1)])
+B_cards = list(map(int, inputs[1:]))
+
+A_cards= cards - set(B_cards)
+A_cards = list(A_cards)
+
+A_cards.sort()
+B_cards.sort()
+idx = 0
+answer = 0
+for i in B_cards:
+    fail = False
+    while i > A_cards[idx]:
+        if idx+1 < n:
+            idx+=1
+        else:
+            fail = True
+            break
+    if fail:
+        break
+    
+    else:
+        answer+=1
+
+print(answer)
+
+
+    
