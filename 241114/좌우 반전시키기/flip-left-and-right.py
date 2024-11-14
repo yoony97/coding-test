261721e72abf1fa84dede45b0d0c9e7e4a6249b9
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-14 12:14:18 +0900

    Title: 좌우 반전시키기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/좌우 반전시키기/flip-left-and-right.py
+++ 241114/좌우 반전시키기/flip-left-and-right.py
@@ -2,12 +2,17 @@ n = int(input())
 arr = list(map(int, input().split()))
 cnt = 0
 for i in range(1, n-1):
-    #print(arr)
     if arr[i-1] == 0:
         arr[i-1] = int(not arr[i-1])
         arr[i+1] = int(not arr[i+1])
         arr[i] = int(not arr[i])
         cnt += 1
+
+if arr[n-1] == 0:
+    arr[n-2] = int(not arr[n-2])
+    arr[n-1] = int(not arr[n-1])
+    cnt += 1
+
 
 if sum(arr) != n:
     cnt = -1
