79b063132d10f06024fd8d1d6e04159be3d23c9c
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-18 13:28:49 +0900

    Title: 함수를 이용한 윤년 판별, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241118/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 241118/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,7 +1,7 @@
 def is_magic(year):
-    if year%4 == 0 and year%100 != 0:
+    if year%100 != 0 and year%4 == 0:
         return True
-    if year%400 != 0 and year%100 == 0:
+    if year%100 == 0 and year%400 != 0:
         return True
     return False
 
