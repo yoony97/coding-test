eb744871bd287fb510249fb83c2b554fcf4e277e
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-20 13:06:03 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -5,7 +5,7 @@ B = list(map(int, input().split()))
 def isSame(A,B):
     if B[0] in A:
         idx = A.index(B[0])
-        if len(A[idx:]) != len(B):
+        if len(A[idx:]) < len(B):
             return "No"
         for i in range(len(B)):
             if A[idx+i] != B[i]:
