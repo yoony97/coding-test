3fa98229d0bdcade92efe5c6bb0b9b36e3e3764a
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-20 13:20:01 +0900

    Title: 그 계절, 그 날, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/그 계절, 그 날/that-season-that-day.py
+++ 241120/그 계절, 그 날/that-season-that-day.py
@@ -1,31 +1,44 @@
 # 4 6 9 11 = 30  else 31 
 y, m, d = tuple(map(int, input().split()))
 
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
 def exist_month30(m, d):
     if d <=28:
         return True
     else:
         return False  
 
-def exist_month31(m, d):
+def exist_month31(m, d, leaf):
 	
     if m == 2:
-        if d >= 29:
-            return False
+        if leaf:
+            if d > 29:
+                return False
+            else:
+                return True
         else:
-            return True
+            if d > 28:
+                return False
+            else:
+                return True
     if d <= 31:
         return True
     else:
         return False
 
 ans = False
-
+leaf = is_leap_year(y)
 
 if m == 4 or m == 6 or m ==  9 or m == 11:
     ans = exist_month30(m,d)
 else:
-    ans = exist_month31(m,d)
+    ans = exist_month31(m,d, leaf)
 
 if ans:
     if 3 <= m <= 5:
