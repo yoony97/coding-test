ea50fe33aaf722421381f1f0a28eb2c91de09d52
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-20 12:43:29 +0900

    Title: 사칙연산 함수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241120/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,21 @@
+m, op, n = input().split()
+m, n = int(m), int(n)
+
+if op =='+':
+    answer = m+n
+    print(f"{m} {op} {n} = {answer}")
+
+elif op =='-':
+    answer = m-n
+    print(f"{m} {op} {n} = {answer}")
+
+elif op =='*':
+    answer = m*n
+    print(f"{m} {op} {n} = {answer}")
+
+elif op =='/':
+    answer = m//n
+    print(f"{m} {op} {n} = {answer}")
+
+else:
+    print('false')
