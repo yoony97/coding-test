aca41db78e0dcc8f01c721bd64cbe8dcb7480580
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-20 13:01:32 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 241120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,13 +1,15 @@
 _ = input()
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-idx = A.index(B[0])
 isSub = True
-for i in range(len(B)):
-    if B[i] != A[idx+i]:
-        isSub=False
-        break
-
+if B[0] in A:
+    idx = A.index(B[0])
+    for i in range(len(B)):
+        if B[i] != A[idx+i]:
+            isSub=False
+            break
+else:
+    isSub = False
 
 if isSub:
     print('Yes')
