44946ba27c508940a2159451d1b83eac8e1d6e16
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-22 13:23:43 +0900

    Title: 상품 코드, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241122/상품 코드/product-code.py
+++ 241122/상품 코드/product-code.py
@@ -2,8 +2,8 @@ dname = 'codetree'
 dcode = 50
 
 temp = (dname, dcode)
-print(f"product {temp[1]} is {temp[2]}")
+print(f"product {temp[0]} is {temp[1]}")
 
 a,b = input().split()
 temp = (a,b)
-print(f"product {temp[1]} is {temp[2]}")
+print(f"product {temp[0]} is {temp[1]}")
