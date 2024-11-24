3b3d00e22809c9a8873632cb65317aaa89240837
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-24 10:23:10 +0900

    Title: 국영수 순이지, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241124/국영수 순이지/korean-english-math-order.py
+++ 241124/국영수 순이지/korean-english-math-order.py
@@ -4,7 +4,7 @@ for _ in range(n):
     name, kor, eng, ma = input().split()
     li.append((name, int(kor), int(eng), int(ma)))
 
-li.sort(key = lambda x: (x[1], x[2]))
+li.sort(key = lambda x: (x[1], x[2],x[3]))
 
 for i in li[::-1]:
     print(' '.join([str(s) for s in i]))
