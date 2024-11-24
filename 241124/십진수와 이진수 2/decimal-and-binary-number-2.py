b06ba3c97e0b1a04d82a041b866c5475ff0387a3
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-24 12:25:15 +0900

    Title: 십진수와 이진수 2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241124/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 241124/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -3,14 +3,14 @@ ans = 0
 c = 1
 li = []
 
-for i in N[::1]:
+for i in N[::-1]:
     ans += i*c
     c *= 2
 
 ans = ans*17
 
 while True:
-    if ans < 1:
+    if ans < 2:
         li.append(ans)
         break
     else:
