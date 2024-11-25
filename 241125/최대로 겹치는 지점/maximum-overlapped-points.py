b48208027e81f8319c113fc19392dc64594b4266
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-25 11:25:16 +0900

    Title: 최대로 겹치는 지점, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241125/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 241125/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -8,14 +8,13 @@ for i in lines:
     S.append((i[0], 1))
     S.append((i[1], -1))
 
-S.sort()
+S.sort(key = lambda x : (x[0], -x[1]))
 
 overlap = 0
 max_overlap = 0
 
 for (p, s) in S:
-    #print(overlap, p ,s)
     overlap += s
     max_overlap = max(max_overlap, overlap)
 
-print(max_overlap+1)
+print(max_overlap)
