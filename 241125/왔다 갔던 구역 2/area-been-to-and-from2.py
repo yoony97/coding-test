1fc83058e16c6e6cfe0d8ecba400c9a52299fafa
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-25 11:37:10 +0900

    Title: 왔다 갔던 구역 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241125/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+current = 0
+li = []
+offset = float('inf')
+
+for _ in range(n):
+    x, direct = input().split()
+    x = int(x)
+    li.append((x, direct))
+    if direct =='L':
+        x = -x
+    current += x
+    offset = min(current, offset)
+
+ans = [0]*1000
+current = abs(offset)
+for x, direct in li:
+    if direct == 'R':
+        for i in range(current, current+x+1):
+            ans[i] += 1
+    else:
+        for i in range(current, current-x-1, -1):
+            ans[i] += 1
+cnt = 0
+for i in ans:
+    if i >=2:
+        cnt += 1
+print(cnt)
