7684c7fde2f65bd47a450af2956f43ffc942f4c0
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-26 12:11:04 +0900

    Title: 벌금은 누구에게, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241126/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,11 @@
+ N, M, K = map(int, input().split())
+ students = [0]*N
+ ans = -1
+ for i in range(M):
+    idx = int(input()) - 1
+    students[idx] += 1
+    if students[idx] >= K:
+        ans = idx+1
+        break
+
+print(ans)
