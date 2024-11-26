106fba0ad2d5593359b5215e31e598e7d40e5c35
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-26 12:11:40 +0900

    Title: 벌금은 누구에게, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/벌금은 누구에게/who-will-pay.py
+++ 241126/벌금은 누구에게/who-will-pay.py
@@ -1,7 +1,7 @@
- N, M, K = map(int, input().split())
- students = [0]*N
- ans = -1
- for i in range(M):
+N, M, K = map(int, input().split())
+students = [0]*N
+ans = -1
+for i in range(M):
     idx = int(input()) - 1
     students[idx] += 1
     if students[idx] >= K:
