8c06c325cbe37f4cbdb7dd18070732f31cdc4a98
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-26 11:30:46 +0900

    Title: 연속되는 수 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241126/연속되는 수 2/continuous-number2.py
+++ 241126/연속되는 수 2/continuous-number2.py
@@ -3,10 +3,12 @@ inputs = sys.stdin.read().strip().split("\n")
 n = int(inputs[0])
 inputs = list(map(int, inputs[1:]))
 
+ans = 0
 cnt = 1
-
 for i in range(1,n):
-    if inputs[i-1] != inputs[i]:
+    if inputs[i-1] == inputs[i]:
         cnt += 1
-
-print(cnt)
+    else:
+        ans = max(ans, cnt)
+        cnt = 1
+print(ans)
