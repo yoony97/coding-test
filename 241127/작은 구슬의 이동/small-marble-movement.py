1dd31bf3ca1c351c8eaff6a0dac793123bded296
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-27 11:31:48 +0900

    Title: 작은 구슬의 이동, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241127/작은 구슬의 이동/small-marble-movement.py
+++ 241127/작은 구슬의 이동/small-marble-movement.py
@@ -2,27 +2,27 @@ n, t = map(int, input().split())
 y, x, d = input().split()
 
 x, y = int(x), int(y)
-current = (x, y)
+current = (y, x)
 dx = [0, 1, -1, 0]
 dy = [1, 0, 0, -1]
 if d == "U":
-    dn = 0
-if d == "D":
     dn = 3
+if d == "D":
+    dn = 0
 if d == "R":
     dn = 1
 if d == "L":
     dn = 2
 
 for i in range(t):
-    x, y = current
+    y, x = current
     cx = x + dx[dn]
     cy = y + dy[dn]
     
     if not (1 <= cx <= n and 1 <= cy <= n):
         dn = 3 - dn
     else:
-        current = (cx, cy)
-#    print(current)
+        current = (cy, cx)
+    #print(current)
 
-print(current[1], current[0])
+print(current[0], current[1])
