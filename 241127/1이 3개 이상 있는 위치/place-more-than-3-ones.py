411b17f9b4f77f0b1a6ecc2e42077b22de3a5ab2
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-27 10:59:06 +0900

    Title: 1이 3개 이상 있는 위치, Time: 58ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241127/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+dx = [1, 0, -1, 0]
+dy = [0, -1, 0, 1]
+
+li = []
+for _ in range(n):
+    li.append(list(map(int,input().split())))
+ans = 0
+for y in range(n):
+    for x in range(n):
+        cnt = 0
+        for d in range(4):
+            cx = x + dx[d]
+            cy = y + dy[d]
+            
+            if 0 <= cx < n and 0 <= cy < n and li[cx][cy] == 1:
+                cnt+= 1
+        ans = max(cnt, ans)
+
+print(ans)
