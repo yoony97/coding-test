0ee42a75cf230c6f66d325653d2a5596300bef28
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-28 12:00:21 +0900

    Title: 이상한 진수 2, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241128/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,20 @@
+def calculate(s):
+    result = 0
+    n = 1
+    for i in s[::-1]:
+        i = int(i)
+        result += i*n
+        n *= 2 
+    return result
+
+N = [str(i) for i in input()]
+ans = 0
+for i in range(len(N)):
+    CN = [i for i in N]
+    if CN[i] == '1':
+        CN[i] = '0'
+    if CN[i] == '0':
+        CN[i] = '1'
+    ans = max(ans, calculate(CN))
+
+print(anx)
