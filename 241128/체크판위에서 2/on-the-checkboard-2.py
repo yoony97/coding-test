ced05cbdb003c5a0ad6424bf9ab57526f3995304
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-28 14:16:31 +0900

    Title: 체크판위에서 2, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241128/체크판위에서 2/on-the-checkboard-2.py
+++ 241128/체크판위에서 2/on-the-checkboard-2.py
@@ -6,17 +6,15 @@ for i in range(R):
 #룰2: 현재 위치보다 한칸 이상 오른쪽에 있는 위치 & 현재 위치에서 적어도 한칸 이상 아래쪽에 있는 위치
 #룰3:  도달한 위치가 2 곳
 current = maps[0][0]
-paths = []
 cnt = 0
-for i in range(1,R):
-    for j in range(1,C):
-        if maps[i][j] != maps[0][0]:
-            path = [(0,0), (i,j)]
-            for k in range(i+1, R-1):
-                for l in range(j+1, C-1):
-                    if maps[i][j] != maps[k][l]:
+for row in range(1,R-1):
+    for col in range(1,C-1):
+        #새로운 점프 위치, 점프는 총 2번 밖에 못함
+        if maps[row][col] != maps[0][0]:
+            for new_row in range(row+1, R-1):
+                for new_col in range(col+1, C-1):
+                    if maps[row][col] != maps[new_col][new_row] and maps[new_col][new_row] != maps[R-1][C-1]:
+                        #print(row+1, col+1, new_row+1, new_col+1)
                         cnt += 1
-            
-#print(paths)
-print(cnt)
 
+print(cnt)
