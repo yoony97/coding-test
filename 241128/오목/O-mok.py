e6f515765d2dcbd1ffb84373c3557f7c3c44a333
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-28 13:28:02 +0900

    Title: 오목, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241128/오목/O-mok.py
+++ 241128/오목/O-mok.py
@@ -16,7 +16,9 @@ def solve():
                     if current != maps[i][k]:
                         iswin = False
                         break
+                
                 if iswin:
+                    #print("ㅡ")
                     return i, (j+k)//2, current
                 
                 iswin = True    
@@ -26,15 +28,16 @@ def solve():
                         iswin = False
                         break
                 if iswin:
+                    #print("|")
                     return (i+k)//2, j, current
                 
                 iswin = True
-                
                 for k in range(1,5):
                     if current != maps[i+k][j+k]:
                         iswin = False
                         break
                 if iswin:
+                    #print("\\")
                     #print(i+k, j+k)
                     return (i+k+2)//2, (j+k+2)//2, current
                 
@@ -42,11 +45,14 @@ def solve():
                 
                 iswin = True
                 for k in range(1, 5):
-                    if not(0 <= i+k < N and 0 <= j-k < N) and current != maps[i+k][j-k]:
+                    #범위에 벗어나고 current
+                    if not(0 <= i+k < N and 0 <= j-k < N) or current != maps[i+k][j-k]:
                         #print("!",i+k, j-k, current, maps[i+k][j-k])
                         iswin = False
                         break
+
                 if iswin:
+                    #print("/")
                     return (i+(i+5))//2, (j+(j-4))//2, current
                 
 
