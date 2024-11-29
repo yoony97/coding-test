508c30c72c91c94d99d29caa2e5772ae1c2d156e
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-29 12:52:20 +0900

    Title: 바구니 안의 사탕 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241129/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241129/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,17 +1,18 @@
 N, K = map(int,  input().split())
-MAX_LEN = 100
+MAX_LEN = 200
 candy = [0]*(MAX_LEN+1)
 
 for _ in range(N):
     c,p = map(int,  input().split())
-    candy[p] = c
+    candy[p] += c
 
 ans = 0
-
+#print(candy)
 for i  in range(K,MAX_LEN-K+1):
     val  = 0
     for j in range(i-K, i+K+1):
         val +=  candy[j]
+    #print(val, i-K, i+K+1)
     ans = max(val, ans)
 
 print(ans)
