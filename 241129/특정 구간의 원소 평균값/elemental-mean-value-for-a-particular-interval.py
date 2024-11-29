ab57301b30e1c059d24873c5b2835f42d28829cd
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-29 12:14:52 +0900

    Title: 특정 구간의 원소 평균값, Time: 55ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 241129/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 241129/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -2,14 +2,12 @@ N= int(input())
 li = list(map(int, input().split()))
 
 cnt = 0
-for start in range(N+1):
-    for end in range(start,N+1):
-        #print(start, end)
-        s =  sum(li[start:end])
-        l = len(li[start:end])
-        if l == 0:
-            l = 1
-        m  = s / l
+for start in range(N):
+    for end in range(start,N):
+        s = 0
+        for i in range(start, end+1):
+            s += li[i]
+        m = s / (end-start+1)
         if float(m).is_integer() and int(m) in li:
             
             cnt += 1
