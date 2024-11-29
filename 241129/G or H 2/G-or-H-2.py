d6dff61dfaed353c2e0d67d1b3d76c2dd87a9e70
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-29 13:24:40 +0900

    Title: G or H 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241129/G or H 2/G-or-H-2.py
+++ 241129/G or H 2/G-or-H-2.py
@@ -9,14 +9,15 @@ for i in range(N):
     
 max_dist = 0
 li = li[:max_p+1]
-
-for start in range(max_p):
-    for end in range(start, max_p):
+#print(li)
+for start in range(max_p+1):
+    for end in range(start, max_p+1):
         H_cnt = 0
         G_cnt = 0
         isfirst = False
         s_idx =  0
-        for i in range(start,end):
+        
+        for i in range(start,end+1):
             if li[i] == "G":
                 G_cnt += 1 
                 
@@ -24,11 +25,11 @@ for start in range(max_p):
                 H_cnt += 1
                 
         if G_cnt != 0  and  H_cnt == 0:
-            for s_idx in range(start, end):
+            for s_idx in range(start, end+1):
                 if li[s_idx] ==  'G':
                     break
             
-            for e_idx in range(end, start, -1 ):
+            for e_idx in range(end, start-1, -1 ):
                 if li[e_idx] ==  'G':
                     break
             
@@ -36,11 +37,11 @@ for start in range(max_p):
             max_dist = max(max_dist, dist)
 
         if G_cnt == 0  and  H_cnt != 0:
-            for s_idx in range(start, end):
+            for s_idx in range(start, end+1):
                 if li[s_idx] ==  'H':
                     break
             
-            for e_idx in range(end, start, -1 ):
+            for e_idx in range(end, start-1, -1 ):
                 if li[e_idx] ==  'H':
                     break
             
@@ -49,12 +50,12 @@ for start in range(max_p):
             max_dist = max(max_dist, dist)
 
         if G_cnt != 0 and H_cnt != 0 and G_cnt == H_cnt :
-            for s_idx in range(start, end):
+            for s_idx in range(start, end+1):
                 if li[s_idx] !=  0:
                     break
 
 
-            for e_idx in range(end, start, -1 ):
+            for e_idx in range(end, start-1, -1 ):
                 if li[e_idx] !=  0:
                     break
 
