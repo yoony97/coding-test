ff176e0971e4310fa5d4bf98e7c84b2af325e922
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-29 11:42:33 +0900

    Title: G or H 3, Time: 133ms, Memory: 17MB, Status: Passed - CodeTree

--- 241129/G or H 3/G-or-H-3.py
+++ 241129/G or H 3/G-or-H-3.py
@@ -1,12 +1,12 @@
 N, K = map(int, input().split())
-max_len = 20#W10000
+max_len = 10000
 li = [0]*(max_len+1)
 for i in range(N):
     p, flag = input().split()
     if flag == 'G':
-        li[int(p)-1] = 1
+        li[int(p)] = 1
     if flag == 'H':
-        li[int(p)-1] = 2
+        li[int(p)] = 2
     
 ans =  0
 #print(li)
