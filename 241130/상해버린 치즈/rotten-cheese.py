1866057921ca46d0bf46900521037aae7ef26778
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-30 13:38:03 +0900

    Title: 상해버린 치즈, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241130/상해버린 치즈/rotten-cheese.py
+++ 241130/상해버린 치즈/rotten-cheese.py
@@ -1,7 +1,7 @@
 N, M, D, S = map(int, input().split())
 history = [] #치즈 먹은 기록
 info = [] # 아픈 기록
-
+cheese = set([i for i in range(1, M+1)])
 #상한 치즈를 먹은 최대 사람의 수
 
 for i in range(D):
@@ -22,11 +22,18 @@ for i in range(S):
 coodinate = set()
 for i in range(S):
     sp,st = info[i]
+    #st 기분으로 후보군 찾아보기
+    #st 보다 작은
+    eat = set() 
     for j in range(D):
         p, m, t = history[j]
         if sp == p and t < st:
             coodinate.add(m)
-
+            eat.add(m)
+    #안먹은 치즈
+    noteat = cheese - eat
+    #안먹은 치즈가 후보군에 포함될 순 없음
+    coodinate = coodinate - noteat
 #i가 상한 치즈일 떄, 몇명이 먹었을까
 ans = 0
 for coord in list(coodinate):
