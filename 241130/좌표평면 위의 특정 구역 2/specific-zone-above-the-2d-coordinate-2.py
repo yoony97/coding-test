632ddc7a17ec6e596c43049799c45cc71ecaf127
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-11-30 12:23:05 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241130/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 241130/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -2,13 +2,17 @@ N = int(input())
 points = []
 
 def get_box(li):
-    p1, p2, p3 = li
-    
-    min_x = min(p1[0], p2[0], p3[0])
-    min_y = min(p1[1], p2[1], p3[1])
+    min_x = float('inf')
+    min_y = float('inf')
 
-    max_x = max(p1[0], p2[0], p3[0])
-    max_y = max(p1[1], p2[1], p3[1])
+    max_x = 0
+    max_y = 0
+    for i in li:
+        min_x = min(i[0],  min_x)
+        min_y = min(i[1], min_y)
+
+        max_x = max(i[0], max_x)
+        max_y = max(i[1], max_y)
 
     return (max_x - min_x)*(max_y - min_y)
 
@@ -27,4 +31,5 @@ for i in range(N):
         p.append(points[j])
     area = get_box(p)
     ans = min(area, ans)
+
 print(ans)
