575348902f3893691c905467401b4c8829a6df2f
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-02 12:45:51 +0900

    Title: 3개의 선 2, Time: 107ms, Memory: 20MB, Status: Runtime Error - CodeTree

--- 241202/3개의 선 2/three-lines-2.py
+++ 241202/3개의 선 2/three-lines-2.py
@@ -1,83 +1,44 @@
+from itertools import combinations
+
 N = int(input())
 points = []
 Xs = set()
 Ys = set()
 
-for i in range(N):
-    a, b = map(int,input().split())
-    points.append((a,b))
-    Xs.add(a)
-    Ys.add(b)
+def check(xs, ys): 
+    valid = [False]*N
+    for x in xs:
+        for i in range(N):
+            if x == points[i][0]:
+                valid[i] = True
+    
+    for y in ys:
+        for i in range(N):
+            if y == points[i][1]:
+                valid[i] = True
+    
+    for i in valid:
+        if not i:
+            return False
+    return True
 
-
-def select(X, n):
-    coord  = list()
-    if n == 1:    
-        for i in range(len(X)):
-            coord.append([X[i]])
-
-    if n == 2:    
-        for i in range(len(X)):
-            for j in range(i+1, len(X)):
-                coord.append([X[i],X[j]])
-    if n == 3:
-        for i in range(len(X)):
-            for j in range(i+1, len(X)):
-                for k in range(j+1, len(X)):
-                    coord.append([X[i],X[j], X[k]])        
-    return coord
-
-def solve(cxss, cyss):
-    for cxs in cxss:
-        for cys in cyss:
-            if check(cxs, cys):
+def solve(nx, ny):
+    for xs in combinations(range(11),nx):
+        for ys in combinations(range(11),ny):
+            if check(xs,ys):
                 return True
     return False
 
 
-def check(cx, cy):
-    remains = []
-    for p in points:
-        iscrossed = False
-        for x in cx:
-            if x == p[0]:
-                iscrossed = True
-                break
-        
-        if not iscrossed:
-            remains.append(p)
+for i in range(N):
+    a, b = map(int,input().split())
+    points.append((a,b))
 
-    for r in remains:
-        iscrossed = False
-        for y in cy:
-            if r[1] == y:
-                iscrossed = True
-                break
-        if not iscrossed:
-            return  False
-    return True
 
-            
-ans = 0
-Xs = list(Xs)
-Ys = list(Ys)
-
-for nx in range(1,4): # nx: x와 평행한 직선 개수
-    ny = 3 -  nx      # ny: y와 평행한 직선 개수
-    cxss = select(Xs, nx)
-    cyss = select(Ys, ny)
-    if solve(cxss, cyss):
-        ans = 1 
+for nx in range(1, 4):
+    ny = 3 - nx
+    if solve(nx, ny):
+        ans = 1
         break
-    
+
 print(ans)
-        
-    
-        
-    
-
-
-        
-
-
-
