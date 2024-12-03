c154d59315276248bb8782b06d97dccd5da18d5b
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 14:51:06 +0900

    Title: 독서실의 거리두기 4, Time: 69ms, Memory: 17MB, Status: Passed - CodeTree

--- 241203/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 241203/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,29 +1,30 @@
 N = int(input())
 li = list(map(int, input()))
 ans = 0
+
+def min_dist():
+    dist = N 
+    # 둘 다 1인 곳에 대해
+    # 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+    for i in range(N):
+        for j in range(i + 1, N):
+            if li[i] == 1 and li[j] == 1:
+                dist = min(dist, j - i)
+    return dist
+
+
 for p1 in range(N):
-    for p2 in range(N):
+    for p2 in range(p1+1,N):
         #새로 배정받은 자리 p1, p2
         if li[p1] == 1 or li[p2] == 1:
             continue
-        
         li[p1] = 1
         li[p2] = 1
         #print(''.join([str(i) for i in li]))
-        min_dist = float('inf')
-        for i in range(N):
-            isend = True
-            for j in range(i+1, N):
-                if li[i] == 1 and li[j] == 1:
-                    dist = j - i
-                    min_dist = min(min_dist, dist)
-                    isend = False
-                    break
-            if li[i] == 1 and isend:
-                dist = N - i
-                min_dist = min(min_dist, dist)
+        
+        dist = min_dist()
                 
-        ans = max(min_dist, ans)
+        ans = max(dist, ans)
         li[p1] = 0
         li[p2] = 0
     
