6eba12ac7880477b2eb6942d992d8c33ae6de611
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 17:51:09 +0900

    Title: 구간 잘 나누기, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/구간 잘 나누기/divide-sections-well.py
+++ 241203/구간 잘 나누기/divide-sections-well.py
@@ -1,20 +1,22 @@
 N, M = map(int, input().split())
 li = list(map(int, input().split()))
 
-#정답이 될지 안될지 가정해봐야하나??
-for i in range(min(li), sum(li)+1):
-    #i가 정답일 때:
-    m = 0 # 격자 개수
-    s = 0 # 격자 안에 있는 수의 합
-    for a in range(N):
-        if i < s + li[a]: #우리가 정한 정답 보다 합이 클 때
-            m += 1
-            s = li[a]
+def can_divide(max_sum):
+    m = 1  # 초기 구간 개수 (구간 1개로 시작)
+    s = 0  # 현재 구간의 합
+
+    for num in li:
+        if s + num > max_sum:  # 현재 구간에 추가할 수 없으면
+            m += 1            # 새로운 구간 추가
+            s = num           # 새로운 구간의 합 초기화
+            if m > M:         # 구간 개수가 초과되면 실패
+                return False
         else:
-            s += li[a]
-    
-    if m <= M-1:
+            s += num          # 현재 구간에 추가
+    return True
+
+
+for i in range(max(li), sum(li)):
+    if can_divide(i):            
         print(i)
         break
-
-    
