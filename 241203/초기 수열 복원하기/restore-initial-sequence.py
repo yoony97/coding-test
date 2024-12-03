6527f64a972116b18c3989284b344da235264268
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 15:37:11 +0900

    Title: 초기 수열 복원하기, Time: 5062ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 241203/초기 수열 복원하기/restore-initial-sequence.py
+++ 241203/초기 수열 복원하기/restore-initial-sequence.py
@@ -3,12 +3,15 @@ from itertools import permutations
 N = int(input())
 li = list(map(int, input().split())) #[a+b, b+c, c+d, d+e]
 
-for i in permutations(range(1, N+1)):
-    isfind = True
-    for p in range(1, N-1):
-        if not(i[p] + i[p-1] == li[p-1]):
-            isfind = False
-            break
-    if isfind:
-        print(' '.join([str(a) for a in i]))
-        break
+def solve():
+    for i in permutations(range(1, N+1)):
+        isfind = True
+        for p in range(1, N-1):
+            if not(i[p] + i[p-1] == li[p-1]):
+                isfind = False
+                break
+        if isfind:
+            print(' '.join([str(a) for a in i]))
+            return 
+
+solve()
