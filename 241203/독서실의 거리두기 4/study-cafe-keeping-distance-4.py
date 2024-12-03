d2167890890b9799ab6d00d90773cd3a01829fce
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 14:41:10 +0900

    Title: 독서실의 거리두기 4, Time: 54ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241203/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,24 @@
+N = int(input())
+li = list(map(int, input()))
+ans = 0
+for p1 in range(N):
+    for p2 in range(N):
+        #새로 배정받은 자리 p1, p2
+        if li[p1] == 1 or li[p2] == 1:
+            continue
+        
+        li[p1] = 1
+        li[p2] = 1
+        #print(''.join([str(i) for i in li]))
+        min_dist = float('inf')
+        for i in range(N):
+            for j in range(i+1, N):
+                if li[i] == 1 and li[j] == 1:
+                    dist = j - i
+                    min_dist = min(min_dist, dist)
+                    break
+        ans = max(min_dist, ans)
+        li[p1] = 0
+        li[p2] = 0
+    
+print(ans)
