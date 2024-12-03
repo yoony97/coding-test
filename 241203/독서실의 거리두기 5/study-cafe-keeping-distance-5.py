0575de7b6ef947627518fd1d0632b8afd55f1a30
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 12:34:05 +0900

    Title: 독서실의 거리두기 5, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,21 +1,32 @@
 N = int(input())
 arr = list(map(int, str(input())))
-ans = 0 
+ans = 0
+idx = 0
+min_dist = float('inf')
 for current in range(N):
     if arr[current] == 1:    
-        isfind = False
+        isfind = False 
         for nxt in range(current+1, N):
             if arr[nxt] == 1:
                 isfind = True
                 break
         if isfind:
-            ans = max((nxt-current)//2, ans) 
+            if ans < (nxt-current)//2:
+                idx = nxt
+                ans = max((nxt-current)//2, ans) 
+
         else:
-            ans = max(N-current -1, ans)
+            if ans < N-current - 1:
+                ans = max(N-current -1, ans)
+                idx = N-1
 
-print(ans)
+arr[idx] = 1
 
-        
-                
+for current in range(N):
+    if arr[current] == 1:    
+        for nxt in range(current+1, N):
+            if arr[nxt] == 1:
+                min_dist = min(nxt-current, min_dist)
+                break
 
-        
+print(min_dist)
