a27b05b3ef0bf6fd9999ff9f7749dce97752bbbb
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 14:10:06 +0900

    Title: 훌륭한 점프, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241203/훌륭한 점프/great-jump.py
@@ -0,0 +1,26 @@
+N, K = map(int, input().split())
+rocks = list(map(int, input().split()))
+current = 0
+ans = float('inf')
+paths = []
+for i in range(max(rocks), 0, -1):
+    path = []
+    for idx, rock in enumerate(rocks):
+        if rock <= i:
+            path.append(idx)
+    paths.append(path)
+
+print(paths)
+
+for path in paths:
+    ispossible = True
+    if len(path) != 1:
+        for i in range(1, len(path)):
+            if not(path[i] - path[i-1] <= K):
+                ispossible = False
+                break
+        if ispossible:
+            #print(max([rocks[k] for k in path]))
+            ans = min(max([rocks[k] for k in path]), ans)
+
+print(ans)
