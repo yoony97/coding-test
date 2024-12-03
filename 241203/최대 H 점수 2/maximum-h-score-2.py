db768a02969db55a2206958a05aeadf7963cbb73
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-03 17:26:18 +0900

    Title: 최대 H 점수 2, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241203/최대 H 점수 2/maximum-h-score-2.py
+++ 241203/최대 H 점수 2/maximum-h-score-2.py
@@ -1,6 +1,4 @@
-#최대 L개의 서로 다른 원소의 값을 1씩 올려 H 점수를 최대로 만드는 프로그램을 작성해 보세요.
-#H 점수: 특정 수열이 주어졌을 때, H 이상인 숫자의 수가 H개 이상인 것을 만족하는 H 중 최댓값을 의미합니다.
-from itertools import combinations
+#중간 값을 이용해볼까?
 
 def H_score(li):
     max_H = 0
@@ -16,11 +14,25 @@ def H_score(li):
 
 N, L = map(int, input().split())
 numbers = list(map(int, input().split()))
-ans = 0
-for i in combinations(range(N), L):
-    copy = [n for n in numbers]
-    for c in i:
-        copy[c] += 1
-    ans = max(ans, H_score(copy))
-    #numbers 중에 L개를 뽑는 방법
-print(ans)
+mid = sorted(numbers)[N//2]
+visited = [False]*N
+
+for i in range(L):
+    best_idx = -1
+    min_diff = float('inf')
+    #중간값으로 부터 가까운 것 부터 + 1 해주자.
+    for a in range(N):
+        if numbers[a] == mid or visited[a]:
+            continue
+        if min_dff > abs(mid - numbers[a]):
+            best_idx = a
+            min_dff = abs(mid - numbers[a])
+
+    # L이 N이랑 같으면, 미드값도 더할 생각해야함
+    if best_idx == -1:
+        best_idx = N//2
+    
+    numbers[best_idx] += 1
+    visited[best_idx] = True
+
+print(H_score(numbers))
