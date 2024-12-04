b2fa8e5949e7844cacb4108435f672288a079a86
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-04 12:59:38 +0900

    Title: 두 선분, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241204/두 선분/two-lines.py
+++ 241204/두 선분/two-lines.py
@@ -1,6 +1,7 @@
 x1, x2, x3, x4 = map(int, input().split())
 
-if x2 < x3 and x4 < x1:
-    print("intersecting")
-else:
+
+if x2 < x3 or x4 < x1:
     print("nonintersecting")
+else:
+    print("intersecting")
