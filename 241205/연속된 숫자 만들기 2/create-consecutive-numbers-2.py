f2479166fcb7f76427cce356661f06557fd6fbc4
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-05 11:49:11 +0900

    Title: 연속된 숫자 만들기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241205/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241205/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,59 +1,31 @@
-#  A - B - C
-#  A - C - B or B - A - C
+MAX_POS = 1000000
+pos = list(map(int, input().split()))
 
-def check(A,B,C):
-    if B - A == 1 and C - B == 1:
-        return True
-    return False
-
-def check2(A,B,C):
-    if B - A == 1 or C - B == 1:
-        return True
-    return False
-
-def dist(A,B):
-    return abs(B-A)
-
-def swap(A,B,C):
-    return sorted([A,B,C])
-
-A, B, C = map(int, input().split())
-
-
-#들어갈 수 있는 후보군 중에서 2번째로 큰 값이 가장 좋아보이는데
-
-cnt = 0
-#for i in range(2):
-while True:
-    if check(A,B,C):
+count = 0
+while (True):
+    a = abs(pos[0] - pos[1])
+    b = abs(pos[1] - pos[2])
+    # 각 둘씩 잡아서 거리가 다 1이면 연속한 수
+    if a == 1 and b == 1:
         break
-    elif check2(A,B,C): # 두개가 연속이고 1개가 떨어져있을 경우,
-        if B-A == 1:
-            li_A = list(range(B+1, C))
-            if len(li_A) >= 2:
-                A = li_A[-2]
-            else:
-                A = li_A[0]
-        elif C-B == 1:
-            li_C = list(range(A+1, B))
-            if len(li_C) >= 2:
-                C = li_C[-2]
-            else:
-                C = li_C[0]
-    else:
-        li_A = list(range(B+1, C))
-        li_C = list(range(A+1, B))
-        if len(li_A) < len(li_C):
-            if len(li_A) < 2:
-                A = li_A[0]
-            else:
-                A = li_A[-2]
+    # 각 둘씩 잡았을 때 하나라도 거리가 2이면 한 번의 연산만 더 수행해주면 완성
+    if a == 2 or b == 2:
+        count += 1
+        break
+    # 각 둘씩 잡았을 때의 거리가 모두 2보다 크면
+    # 거리가 가까운 쪽 안에 반대편 수를 넣어준다.
+    elif a > 2 and b > 2:
+        if a < b:
+            pos[1], pos[2] = pos[0]+2, pos[1]
         else:
-            if len(li_C) < 2:
-                C = li_C[0]
-            else:
-                C = li_C[-2]
-    
-    A, B, C = swap(A,B,C)
-    cnt+= 1
-print(cnt)
+            pos[0], pos[1] = pos[1], pos[1]+2
+
+    elif abs(pos[1] - pos[2]) > 2:
+        pos[0], pos[1] = pos[1], pos[1]+2      
+
+    elif abs(pos[0] - pos[1]) > 2:
+        pos[1], pos[2] = pos[0]+2, pos[1]
+    # print(pos)
+    count += 1
+
+print(count)
