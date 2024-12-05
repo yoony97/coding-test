9a1a720631b055f90e20232d6b916018beff496b
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-05 10:18:13 +0900

    Title: 코딩톡, Time: 39ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241205/코딩톡/coding-talk.py
@@ -0,0 +1,20 @@
+N, M, P  = map(int, input().split())
+TOTAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+people = set(map(str, TOTAL[:N]))
+messages = []
+reader = set()
+for i in range(M):
+    c, u = input().split()
+    u = int(u)
+    messages.append((c, u))
+
+for i in range(N):
+    if i >= P-1:
+        reader.add(messages[i][0])
+
+print(' '.join(people - reader))
+
+
+#읽었다
+# - 읽기만하고, 메세지를 안보낸 경우
+# - 메세지를 보낸 경우,
