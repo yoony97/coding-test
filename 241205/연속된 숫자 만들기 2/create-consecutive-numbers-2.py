f1ad4bc0495748661410d84f0ca5b777d47b81ba
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-05 11:23:29 +0900

    Title: 연속된 숫자 만들기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241205/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 241205/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -19,18 +19,25 @@ dist2 = dist(B,C)
 
 #항상 큰 값으로 이동하게 만들래
 cnt = 0
-
+#for i in range(10):
 while not check(A,B,C):
     if check(A,B,C):
         break
-    if dist1 > dist2:
-        A = (C+B)//2
+    li_A = list(range(A,B+1))
+    li_C = list(range(B,C+1))
+    if len(li_A) == 0:
+        C = li_C[len(li_C)//2]
+    elif len(li_C) == 0:
+        A = li_A[len(li_A)//2]
+    elif len(li_A) > len(li_C):
+        A = li_A[len(li_A)//2]
     else:
-        C = (A+B)//2
+        C = li_C[len(li_C)//2]
+    #print(A,B,C)
     A, B, C = swap(A,B,C)
     #print(A,B,C)
+    #print('-----')
     cnt += 1
 
 print(cnt)
-
 
