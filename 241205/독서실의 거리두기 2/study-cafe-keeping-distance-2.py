d6b923754f3d6211e914e82a7b663f2f3f669c09
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-05 13:16:08 +0900

    Title: 독서실의 거리두기 2, Time: 61ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241205/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,40 @@
+N = int(input())
+st = list(map(int, input()))
+
+def calculate():
+    min_dis = float('inf')
+    for i in range(N):
+        for j in range(i+1,N):
+            if i == j:
+                continue
+            if st[i] == st[j] == 1:
+                min_dis = min(abs(i-j), min_dis)
+                break
+    return min_dis
+                
+
+ans = 0
+#양 끝 중 1개라도 0인 경우를 고민해봐야한다.
+if st[0] == 0:
+    st[0] = 1
+    ans = max(ans, calculate())
+    st[0] = 0
+
+if st[-1] == 0:
+    st[-1] = 1
+    ans = max(ans, calculate())
+    st[-1] = 0
+
+prev = 0
+max_dis = 0
+candidate = (0, N)
+for i in range(1,N):
+    if st[i] == 1:
+        if max_dis < i - prev:
+            candidate = (prev, i)
+            prev = i
+
+st[sum(candidate)//2] = 1
+ans = max(ans, calculate())
+print(ans)
+
