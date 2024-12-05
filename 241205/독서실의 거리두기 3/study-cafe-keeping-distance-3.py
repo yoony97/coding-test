740236f4a2d752f8067d06c648dbdf3c2cbe69d5
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-05 12:13:30 +0900

    Title: 독서실의 거리두기 3, Time: 5072ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 241205/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 241205/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -1,15 +1,24 @@
-def dist(A,B):
-    return B-A
-
 N = int(input())
-st = input()
+st = list(map(str, input()))
 max_dist = 0
+
+def min_dist():
+    min_dis = float('inf')
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+            if st[i] == st[j] == '1':
+                dist = abs(j-i)
+                min_dis = min(dist, min_dis)
+    return min_dis
+
 for i in range(N):
-    
-    for j in range(i+1,N):
-        if st[i] =='1' and st[j] == '1':
-            dis = dist(i,j)
-            max_dist = max(max_dist, dis//2)
-            break
+    if st[i] == '0':
+        st[i] = '1' #새로 투입한 인원
+        m = min_dist()
+        #print(''.join(st), m)
+        max_dist = max(m, max_dist)
+        st[i] ='0'
 
 print(max_dist)
