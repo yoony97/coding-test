f0466b1559f5d50027e9740cdbf6fed5808a4a2f
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-06 14:31:58 +0900

    Title: 수열의 순서 바꾸기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241206/수열의 순서 바꾸기/reorder-sequence.py
+++ 241206/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,29 +1,38 @@
 N =  int(input())
 li = list(map(int,  input().split())) # 숫자가 겹치지 않게 구성된, N개의 수열
-
+target_li = list(range(1,N+1))
 def check(li):
-    #result = True
-    for i in range(1,N):
-        if li[i-1] > li[i]:
-            return False
-    return True
+    results = []
+    #checked = True
+    for i, j in zip(li, target_li):
+        if i == j:
+            results.append(True)
+        else:
+            results.append(False)
+            #checked = False
+    return results
 
 def insert(li, pivot, idx):
     return li[:idx] +[pivot] + li[idx:]
     
+results = check(li)
+LIS = 0
 cnt = 0
-while True:
-    #print(li)
-    if check(li):
-        break
+for i in results:
+    if i:
+        cnt += 1
+    else:
+        LIS = max(LIS, cnt)
+        cnt = 0
 
-    pivot = li.pop(0)
-    target = 0
-    for i in range(N-1):
-        if li[i] == pivot-1:
-            target = i+1
-    li = insert(li, pivot, target)
-    cnt += 1
-print(cnt)
+print(N-LIS+1)
+    
+
 # 선택한 숫자보다 바로 이전 작은 숫자 옆으로 붙이자.
-
+# 맨 뒤로 보내는 경우의 수는 li[0] == N 일 때, 맨 뒤로 보낸다.
+#
+# 1 2 3 5 4 6
+# 2 3 5 1 4 6
+# 3 5 1 2 4 6
+# 5 1 2 3 4 6
+# 1 2 3 4 5 6
