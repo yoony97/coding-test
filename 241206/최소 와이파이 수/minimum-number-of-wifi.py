70b394aab741b1c318ee09f54b1cf1512056657d
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-06 12:56:50 +0900

    Title: 최소 와이파이 수, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241206/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241206/최소 와이파이 수/minimum-number-of-wifi.py
@@ -5,17 +5,15 @@ cnt = 0
 dist = 0
 ans = 0
 for i in range(n):
-    #내가 지나온 m만큼의 거리 까지 사람이 존재하냐?
+    dist += 1
+
     if home[i] == 1:
         cnt += 1
     
-    if dist == m and cnt > 0:
-        #print(f"{i}에 설치")
-        ans += 1
-
-    if dist == (2*m+1):
+    #cnt > 0 이고 
+    if dist > 2*m and cnt > 0:
         dist = 0
-
-    dist += 1
+        cnt = 0
+        ans +=  1
     
 print(ans)
