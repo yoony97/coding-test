6eae4cb519902fb84677882ee7c8020cb3a15590
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-06 15:55:07 +0900

    Title: 스승의 은혜 3, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241206/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 241206/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,7 +7,10 @@ for i in range(N):
 li.sort(key = lambda x: x[0] + x[1])
 ans = 0
 for i in range(N):# i가 반값일 때,
+    current = (li[i][0]//2, li[i][1])
     li[i] = (li[i][0]//2, li[i][1])
+    li.sort(key = lambda x: x[0] + x[1])
+    idx = li.index(current)
     total = 0
     cnt = 0
     for j in range(N):
@@ -16,6 +19,7 @@ for i in range(N):# i가 반값일 때,
             cnt += 1
         else:
             ans = max(ans, cnt)
-    li[i] = (li[i][0]*2, li[i][1])
+            
+    li[idx] = (li[idx][0]*2, li[idx][1])
 
 print(ans)
