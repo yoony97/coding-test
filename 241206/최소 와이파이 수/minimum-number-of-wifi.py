1354e2c63d44a42a28f4e8ff3f26f10b098bef20
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-06 13:06:21 +0900

    Title: 최소 와이파이 수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241206/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241206/최소 와이파이 수/minimum-number-of-wifi.py
@@ -4,18 +4,23 @@ home = list(map(int, input().split()))
 cnt = 0
 dist = 0
 ans = 0
+isexits = False
 if m == 0:
     print(sum(home))
 else:    
     for i in range(n):
-        dist += 1
         if home[i] == 1:
-            cnt += 1
-        if dist > 2*m:
-            dist = 0
-            cnt = 0
+            isexits = True
+        
+        if isexits:
+            dist+=1
+        
+        if dist == 2*m+1:
             ans += 1
-    if cnt > 0:
-        ans += 1
-
+            dist = 0
+            isexits=False
     print(ans)
+
+
+#[2m+1] 마다확인해야함
+# 집이 존재하면 가운데 설치함
