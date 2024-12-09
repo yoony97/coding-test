a7d3f83481137fe9b06d9d138bbb0c34c9ef3d04
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-09 14:29:13 +0900

    Title: 중앙값 계산 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241209/중앙값 계산 2/get-median-2.py
+++ 241209/중앙값 계산 2/get-median-2.py
@@ -2,6 +2,6 @@ N = int(input())
 li = list(map(int, input().split()))
 
 for i in range(N):
-    if li[i]%2 == 1:
+    if (i+1)%2 == 1:
         temp = sorted(li[:i+1])
         print(temp[len(temp)//2], end =' ')
