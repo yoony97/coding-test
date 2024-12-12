6b87752c9f750f69b40fe880b9605ebdfeb6c443
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-12 10:56:23 +0900

    Title: 정수 명령 처리, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241212/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,40 @@
+class Stack:
+    def __init__(self):          # 빈 스택 하나를 생성합니다.
+        self.items = []
+                
+    def push(self, item):        # 스택에 데이터를 추가합니다.
+        self.items.append(item)
+                
+    def empty(self):             # 스택이 비어있으면 True를 반환합니다.
+        return not self.items
+                
+    def size(self):              # 스택에 있는 데이터 수를 반환합니다.
+        return len(self.items)
+        
+    def pop(self):               # 스택의 가장 위에 있는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+            
+        return self.items.pop()
+                
+    def top(self):               # 스택의 가장 위에 있는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("Stack is empty")
+                        
+        return self.items[-1]
+
+n = int(input())
+s = Stack()
+for i in range(n):
+    ops = input().split()
+    if ops[0] ==  'push':
+        s.push(int(ops[1]))
+    if ops[0] ==  'size':
+        print(s.size())
+    if ops[0] ==  'empty':
+        if s.empty():
+            print(1)
+        else:
+            print(0)
+    if ops[0] == 'pop':
+        print(s.pop())
