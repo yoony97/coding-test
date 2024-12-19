f2bbbf64c21d94dafed538c64c142e6a55e7afe0
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-19 10:56:29 +0900

    Title: 트로미노, Time: 67ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241219/트로미노/tromino.py
@@ -0,0 +1,31 @@
+block = [
+    [(0,0), (1,0), (1,1)], 
+    [(0,0), (0,1), (-1,1)], 
+    [(0,0), (1,0), (1,-1)],
+    [(0,0), (0,1), (1, 0)],
+    # 블록 2
+    [(0,0), (0,1), (0,2)],
+    [(0,0), (1,0), (2,0)]
+]
+
+
+N, M = map(int, input().split())
+maps = []
+for _ in range(N):
+    maps.append(list(map(int, input().split())))
+ans = 0
+for cy in range(N):
+    for cx in range(M):
+        for b in block:
+            cnt = 0
+            for (dy, dx) in b:
+                nx = cx + dx
+                ny = cy + dy
+                if 0 <= nx < M and 0 <= ny < N:
+                    cnt += maps[ny][nx]
+                else:
+                    cnt = 0
+                    break
+            ans = max(ans, cnt)
+
+print(ans)        
