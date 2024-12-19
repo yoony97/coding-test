8c124dcd2c494ff496fb93a5c533a8c03bf709c3
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-19 11:20:26 +0900

    Title: 금 채굴하기, Time: 55ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241219/금 채굴하기/gold-mining.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+maps =[]
+
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+def count(cx,cy,k):
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if 0 <= i < n and 0 <= j < n and abs(i - cx) + abs(j - cy) <= k:
+                cnt += maps[i][j]
+    return cnt
+    
+answer = 0
+ans = 0
+for k in range(n//2+1):
+    gold = 0
+    cost = k*k + (k+1)*(k+1)
+    for cx in range(n):
+        for cy in range(n):
+            cnt = count(cx,cy,k)
+            gold = max(cnt, gold)
+    
+    total = gold*m - cost
+    if total > answer:
+        answer = total
+        ans = gold
+
+print(ans)
+
