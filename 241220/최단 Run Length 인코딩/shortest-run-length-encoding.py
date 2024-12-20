918d9ae2abfdc3553db50156c6a59de60b3257fb
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-20 13:15:25 +0900

    Title: 최단 Run Length 인코딩, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241220/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,34 @@
+s = list(map(str, input()))
+n = len(s)
+ans = float('inf')
+answer = ''
+def rshift(s):
+    temp = s[-1]
+    for i in range(n-1, -1, -1):
+        s[i] = s[i-1]
+    s[0] = temp
+
+def cal(s):
+    encoded = ''
+    char = s[0]
+    cnt = 1
+    for i in range(1, n):
+        if char == s[i]:
+            cnt += 1
+        else:
+            encoded += f"{char}{cnt}"
+            char = s[i]
+            cnt = 1
+    encoded += f"{char}{cnt}"
+    return encoded
+
+for i in range(n-1):
+    rshift(s)
+    if ans > len(cal(s)):
+        answer = s
+        ans = len(cal(s))
+    
+print(ans)
+
+
+
