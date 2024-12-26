a513532f0f68b5dd49645f71d4c843056dbb9da1
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-26 12:08:01 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 60ms, Memory: 18MB, Status: Passed - CodeTree

--- 241226/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 241226/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,23 +1,29 @@
+from collections import deque
+
 n, m, t = map(int, input().split())
 arr = []
 start = []
 for _ in range(n):
     arr.append(list(map(int,  input().split())))
 
+
 for _ in range(m):
     x, y = map(int,  input().split())
-    start.append((x-1, y-1))
+    start.append((x-1, y-1))# 0-index
 
-dx = [1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
+start = deque(start)
 
 for _ in range(t):
     next_cont = [[0]*n for _ in range(n)]
     
     while start:
-        x, y = start.pop(0)
-        max_value = 0# arr[x][y]
+        x, y = start.popleft()
+        max_value = -1 # arr[x][y]
         rx, ry = -1, -1
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -25,8 +31,11 @@ for _ in range(t):
                 if max_value < arr[nx][ny]:
                     max_value = arr[nx][ny]
                     rx, ry = nx, ny
+        
         if rx >= 0 and ry >= 0:
             next_cont[rx][ry] += 1
+    
+    start.clear()
 
     for i in range(n):
         for j in range(n):
