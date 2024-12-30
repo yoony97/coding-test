afba1f79911841d0f8f48571c741ce0ceff1bbb5
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-30 12:20:41 +0900

    Title: 뱀은 사과를 좋아해, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241230/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 241230/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -38,11 +38,19 @@ answer = 0
 isFail = False
 for (d, p) in movement:
     dx, dy = get_d(d)
+    if not(0 <= current[0] + dx*int(p) < N and 0 <= current[1] + dy*int(p) < N):
+        isFail = True
+        break
+        
     if not isFail:
+        #print(current, history, current_length, answer)
+        
+
         for _ in range(int(p)):
-            #print(current, history, current_length)
+            #print(current, history, current_length, apples)
             cx, cy = current
             nx, ny = cx + dx, cy + dy
+            
             if 0 <= nx < N and 0 <= ny < N and (nx, ny) not in history:
                 answer += 1
                 current = (nx, ny)
@@ -53,8 +61,10 @@ for (d, p) in movement:
                 else:
                     if current_length > 1:
                         current_length -= 1
-                        while len(history) != current_length:
-                            history.popleft()                        
+            
+                while len(history) != current_length:
+                    history.popleft()                        
+            
             else:
                 answer += 1
                 isFail = True
