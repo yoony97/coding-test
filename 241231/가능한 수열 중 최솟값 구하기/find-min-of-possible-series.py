410c7b6b60d6137debd489c8beb3f55953252bb5
yoony97 <88752851+yoony97@users.noreply.github.com>
2024-12-31 13:25:54 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 5066ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 241231/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 241231/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -3,9 +3,6 @@ target = []
 answer = None
 
 def check(target):
-    #완전 탐색 어떻게 해야할까?
-    #1. 1 ~ M 글자까지 선택
-    #2. 해당 글자의 길이만큼 중복이 있는지 확인
     length = len(target)
     target = ''.join([str(i) for i in target])
     for start in range(length):
@@ -31,7 +28,7 @@ def solve(cnt):
         return
 
     for i in range(4,7):
-        if answer is None:
+        if check(target):
             target.append(i)
             solve(cnt+1)
             target.pop()
