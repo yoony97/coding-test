bd3636774ab6a8165781a2d7376a7ca74764d01e
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-06 11:40:47 +0900

    Title: 알파벳과 사칙연산, Time: 62ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250106/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,27 @@
+expression = input()
+answer = 0
+
+# Write your code here!
+
+def solve(cur, current, prev):
+    global answer
+    if cur == len(expression):
+        answer = max(current, answer)
+        return
+
+    if expression[cur] not in ['-', '*', '+']:
+        for i in range(1, 5):
+            if prev == '-':
+                solve(cur+1, current - i, prev)
+            elif prev == '*':
+                solve(cur+1, current*i, prev)
+            elif prev ==  '+':
+                solve(cur+1, current+i, prev)
+            elif prev == None:
+                solve(cur+1, i, prev)
+         
+    else:
+        solve(cur+1, current, expression[cur])
+
+solve(0, 0, None)
+print(answer) 
