29ecbb892edfdcef30aa41a33ed2c5c485edea18
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-06 11:12:08 +0900

    Title: 수들의 합 최대화하기, Time: 5024ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250106/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 250106/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -7,23 +7,24 @@ ele = []
 def solve(cnt):
     global answer
     if cnt == n:
+        #print(ele)
         answer = max(answer, sum(ele))
         return
     
     for i in range(n):
         for j in range(n):
-            if not rows[i] and not cols[i]:
+            if not rows[i] and not cols[j]:
                 ele.append(grid[i][j])
                 rows[i] = True
-                cols[i] = True
+                cols[j] = True
                 solve(cnt+1)
                 rows[i] = False
-                cols[i] = False
+                cols[j] = False
                 ele.pop()
 
-solve(1)
-if n  == 1:
-    print(grid[0][0])
-else:
-    print(answer)
+solve(0)
+# if n  == 1:
+#     print(grid[0][0])
+# else:
+print(answer)
 # Write your code here!
