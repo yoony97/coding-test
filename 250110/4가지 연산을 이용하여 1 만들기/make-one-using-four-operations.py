4ca6bb2e2beff002b73d0ce376d21a3883d6908b
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-10 14:27:43 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 183ms, Memory: 38MB, Status: Passed - CodeTree

--- 250110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 250110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,24 +1,27 @@
 from collections import deque
 N = int(input())
-q = deque([(N, 0)])
-
+q = deque([(N)])
+cnt = [float('inf')]*(N+10)
+cnt[N] = 0
+answer = float('inf')
 
 while q:
-    num, cnt = q.popleft()
-    target = [num+1, num-1]
-    if num  == 1:
-        print(cnt)
-        break
-    if num%3 == 0:
-        q.append((num//3, cnt+1))
-        target.append(num//3)
-    elif num%2 == 0:
-        target.append(num//2)
-        q.append((num//2, cnt+1))
-    else:
-        q.append((num-1, cnt+1))
-    q.append((num+1, cnt+1))
+    num= q.popleft()    
+    if num%3 == 0 and cnt[num//3] > cnt[num]+1:
+        #나눠지고  현재 num%3의 연산횟수가 cnt[num]+1 보다 크면, 추가
+        q.append((num//3))
+        cnt[num//3] = cnt[num]+1
+    if num%2 == 0 and cnt[num//2] > cnt[num]+1:
+        q.append((num//2))
+        cnt[num//2] = cnt[num]+1
+    
+    if cnt[num-1] > cnt[num]+1:
+        q.append((num-1))
+        cnt[num-1] = cnt[num]+1
+    
+    if num+1 < N+10  and cnt[num+1] > cnt[num]+1:
+        q.append((num+1))
+        cnt[num+1] = cnt[num]+1
 
 
-
-# Write your code here!
+print(cnt[1])
