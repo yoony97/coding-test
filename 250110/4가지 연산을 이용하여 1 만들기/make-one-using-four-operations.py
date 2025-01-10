3a25603d263783c211b02cf35dd678a48e6cc443
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-10 13:58:14 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 1214ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 250110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 250110/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -4,17 +4,17 @@ q = deque([(N, 0)])
 
 while q:
     num, cnt = q.popleft()
-    list(q).sort()
-    q = deque(q)
     if num  == 1:
         print(cnt)
         break
-    q.append((num-1, cnt+1))
-    q.append((num+1, cnt+1))
-    if num%2 == 0:
-        q.append((num//2, cnt+1))
     if num%3 == 0:
         q.append((num//3, cnt+1))
+    if num%2 == 0:
+        q.append((num//2, cnt+1))
+    q.append((num-1, cnt+1))
+    q.append((num+1, cnt+1))
+    
+    
 
 
 # Write your code here!
