bff5bb2737e14625ec4cfe4a51ba212b6f89ede5
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-13 14:24:11 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250113/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 250113/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -3,36 +3,55 @@ import sys
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-
-def dfs(x, y, max_val, min_val, grid, memo):
-    n = len(grid)
-
-    # 도착 지점에 도달했을 때 최댓값과 최솟값 차이를 반환
-    if x == n - 1 and y == n - 1:
-        return max_val - min_val
-
-    # 메모이제이션 (현재 좌표에서 max_val, min_val 상태를 기록)
-    if (x, y, max_val, min_val) in memo:
-        return memo[(x, y, max_val, min_val)]
-
-    # 이동 가능한 방향: 오른쪽, 아래쪽
-    directions = [(0, 1), (1, 0)]
-    min_diff = sys.maxsize
-
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n:
-            new_max = max(max_val, grid[nx][ny])
-            new_min = min(min_val, grid[nx][ny])
-            min_diff = min(min_diff, dfs(nx, ny, new_max, new_min, grid, memo))
-
-    memo[(x, y, max_val, min_val)] = min_diff
-    return min_diff
-
 def min_difference_path(grid):
     n = len(grid)
-    memo = {}
-    return dfs(0, 0, grid[0][0], grid[0][0], grid, memo)
 
-# 실행
-print(min_difference_path(grid))  # 최소 차이 출력
+    # DP 테이블 초기화
+    dp = [[sys.maxsize] * n for _ in range(n)]
+    max_value = [[0] * n for _ in range(n)]
+    min_value = [[0] * n for _ in range(n)]
+
+    # 시작점 초기화
+    dp[0][0] = 0
+    max_value[0][0] = grid[0][0]
+    min_value[0][0] = grid[0][0]
+
+
+    for j in range(1, n):
+        max_value[0][j] = max(max_value[0][j-1], grid[0][j])
+        min_value[0][j] = min(min_value[0][j-1], grid[0][j])
+        dp[0][j] = max_value[0][j] - min_value[0][j]
+
+
+    for i in range(1, n):
+        max_value[i][0] = max(max_value[i-1][0], grid[i][0])
+        min_value[i][0] = min(min_value[i-1][0], grid[i][0])
+        dp[i][0] = max_value[i][0] - min_value[i][0]
+
+    # DP 테이블 채우기
+    for i in range(1, n):
+        for j in range(1, n):
+            # 왼쪽에서 오는 경우
+            left_max = max(max_value[i][j-1], grid[i][j])
+            left_min = min(min_value[i][j-1], grid[i][j])
+            left_diff = left_max - left_min
+
+            # 위쪽에서 오는 경우
+            up_max = max(max_value[i-1][j], grid[i][j])
+            up_min = min(min_value[i-1][j], grid[i][j])
+            up_diff = up_max - up_min
+
+            # 최적 경로 선택
+            if left_diff < up_diff:
+                max_value[i][j] = left_max
+                min_value[i][j] = left_min
+                dp[i][j] = left_diff
+            else:
+                max_value[i][j] = up_max
+                min_value[i][j] = up_min
+                dp[i][j] = up_diff
+
+    return dp[n-1][n-1]
+
+
+print(min_difference_path(grid))
