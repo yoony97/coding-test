6ab66c10f43f1e32b546e38a175f6ed5fba3b758
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-13 11:33:18 +0900

    Title: 피보나치 수, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250113/피보나치 수/fibonacci-number.py
+++ 250113/피보나치 수/fibonacci-number.py
@@ -3,15 +3,14 @@ memo = [-1]*(N+1)
 def fibonnach(n):
     if memo[n] != -1:
         return memo[n]
-    if n == 1:
+    if n <= 2:
         memo[n] = 1
-        return 1
-    if n == 2:
-        memo[n] = 1
-        return 1
+    
     else:
-        return fibonnach(n-1) + fibonnach(n-2)
+        memo[n] = fibonnach(n - 1) + fibonnach(n - 2) 
 
+    return memo[n]
+    
 print(fibonnach(N))
 
 # Write your code here!
