d7d825dffca3f5ee862e64f61521ed082d4309d3
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-13 12:50:02 +0900

    Title: 서로 다른 BST 개수 세기, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250113/서로 다른 BST 개수 세기/number-of-unique-bst.py
@@ -0,0 +1,16 @@
+n = int(input())
+dp = [0]*(n+1)
+dp[1] = 1
+dp[2] = 2
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2] + i-1 
+# Write your code here!
+
+print(dp[n])
+"""
+n = 1 1
+n = 2 2
+n = 3 5
+n = 4/ 5 + (n-1)
+"""
+
