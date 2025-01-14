6df38075ed5b9f052e0d784eca151af598168ec5
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-14 12:12:28 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 111ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250114/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,31 @@
+n = int(input())
+start, end = [], []
+for _ in range(n):
+    a, b = map(int, input().split())
+    start.append(a)
+    end.append(b)
+
+dp = [0]*n
+
+
+def iscross(i, j):
+    a, b = start[i], end[i]  
+    x, y = start[j], end[j]
+
+    if a <= x <=  b or a <= y <= b or x <=a <= y  or  x<=  b  <= y:
+        return True
+    return False
+    
+
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        if not iscross(i,j):
+            dp[i] = max(dp[i], dp[j]+1)
+        
+        
+print(max(dp))
+    
+# Write your code here!
