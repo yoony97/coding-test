9f55f1d533152e6881f40ee93fe7ca0424882cb1
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-14 12:20:45 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 75ms, Memory: 17MB, Status: Passed - CodeTree

--- 250114/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 250114/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -1,29 +1,27 @@
 n = int(input())
-start, end = [], []
+segments= []
 for _ in range(n):
     a, b = map(int, input().split())
-    start.append(a)
-    end.append(b)
+    segments.append((a,b))
+    
+segments.sort(key = lambda x : x[1])
 
-dp = [1]*n
-
+dp = [0]*n
 
 def iscross(i, j):
-    a, b = start[i], end[i]  
-    x, y = start[j], end[j]
-
-    if a <= x <=  b or a <= y <= b or x <=a <= y  or  x<=  b  <= y:
+    a, b = segments[i]
+    x, y = segments[j]
+    return not (b < x or y < a)
+    if a <= x <=  b or a <= y <= b or x <= a <= y  or  x<=  b  <= y:
         return True
     return False
     
-
-
 for i in range(n):
     for j in range(n):
         if i == j:
             continue
         if not iscross(i,j):
-            dp[i] = max(dp[i-1], dp[j]+1)
+            dp[i] = max(dp[i], dp[j]+1)
         
         
 print(max(dp))
