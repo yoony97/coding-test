2a858dfbdca0e6bc776761ea4b7fbf43cbe20943
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-14 11:57:49 +0900

    Title: 2차원 최대 증가 수열, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250114/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 250114/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -2,13 +2,13 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 #점프 # nx > x  and  ny > y and grid[nx][ny] > grid[x][y] 점프 조건ㄴ 
 # Write your code here!
-dp = [[-float('inf')]*n for i in range(n)]
+dp = [[-float('inf')]*m for i in range(n)]
 dp[0][0] = 1
 
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         for nx in range(i+1, n):
-            for ny in range(j+1, n):
+            for ny in range(j+1, m):
                 if grid[nx][ny] > grid[i][j]:
                     dp[nx][ny] = max(dp[nx][ny], dp[i][j]+1)
 
