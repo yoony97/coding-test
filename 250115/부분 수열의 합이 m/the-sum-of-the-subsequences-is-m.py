6d14f43d38e676df1382e402997ac670a3aff529
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-15 12:16:25 +0900

    Title: 부분 수열의 합이 m, Time: 115ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 250115/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+dp = [float('inf')]*(m+1)
+dp[0] = 0
+# Write your code here!
+A.sort()
+for i in range(n):
+    for j in range(m, -1, -1):
+        if j >= A[i]:
+            if dp[j - A[i]] == float('inf'):
+                continue
+            dp[j] = min(dp[j-A[i]]+1, dp[j])
+if dp[m] == float('inf'):
+    print(-1)
+else:
+    print(dp[m])
