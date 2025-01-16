efe12f8dd973fb004d115a9f23ad0414e34bdeb6
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-16 15:15:05 +0900

    Title: 최소 차 분할, Time: 5099ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250116/최소 차 분할/minimum-diff-partition.py
+++ 250116/최소 차 분할/minimum-diff-partition.py
@@ -2,29 +2,24 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 TOTAL = sum(arr)
-dp = [0]*(TOTAL+1)
+dp = [float('inf')]*(TOTAL+1)
+A = []
+hist = []
 
-def backtrack(cur_n, A, hist):
-    global dp
+def backtrack(cur_n):
+    global dp, A, hist
     if cur_n == n:
-        if not A:
-            dp[0] = TOTAL
-        else:
-            dp[sum(A)] = abs(TOTAL-sum(A))
+        dp[sum(A)] = min(dp[sum(A)], abs(TOTAL-sum(A)-sum(A)))
         return
 
     for i in range(n):
         if not i in hist:
             A.append(arr[i])
             hist.append(i)
-            backtrack(cur_n+1, A, hist)
+            backtrack(cur_n+1)
             A.pop()
             hist.pop()
+            backtrack(cur_n+1)
 
-#DP 테이블을 뭘 줘야할까?
-#DP[i]는 A의 합이 i 일 때, |A-B|의 값이라 하면 좀 이상한가?
-
-# Write your code here!
-
-backtrack(0, [], [])
-print(dp)
+backtrack(0)
+print(min(dp))
