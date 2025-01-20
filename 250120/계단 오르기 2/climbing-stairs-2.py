245f6e35d10f605675c90c279ca21ff0395817da
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-01-20 12:58:59 +0900

    Title: 계단 오르기 2, Time: 50ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250120/계단 오르기 2/climbing-stairs-2.py
+++ 250120/계단 오르기 2/climbing-stairs-2.py
@@ -2,15 +2,16 @@ n = int(input())
 coin = [0] + list(map(int, input().split()))
 def max_coins(coins):
     n = len(coins)
-    dp = [[-float('inf')] * 4 for _ in range(n)]
-    dp[0][0] = 0
+    dp = [[0] * 3 for _ in range(n+1)]
     dp[1][0] = coins[0]
+    dp[1][1] = coins[0]
 
-    for i in range(2, n):
-        dp[i][0] = dp[i-2][0] + coins[i-2]
-        for j in range(1, min(i+1, 4)):
-            dp[i][j] = max(dp[i-1][j-1] + coins[i], dp[i-2][j] + coins[i])
-
-    return max(dp[n-1])
+    for i in range(2, n+1):
+        dp[i][0] = dp[i-2][0] + coins[i-1]
+        for j in range(1, min(i, 3)):
+            dp[i][j] = max(dp[i-1][j-1] + coins[i-1], dp[i-2][j] + coins[i-1])
+    
+    #print(dp[n])
+    return max(dp[n])
 
 print(max_coins(coin))
