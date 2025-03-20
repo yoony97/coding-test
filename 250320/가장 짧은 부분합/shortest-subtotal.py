69e44d34f1924fefc7d703bcbe2d2b2ac47d1e1e
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 21:44:38 +0900

    Title: 가장 짧은 부분합, Time: 64ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 250320/가장 짧은 부분합/shortest-subtotal.py
+++ 250320/가장 짧은 부분합/shortest-subtotal.py
@@ -1,17 +1,19 @@
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
+arr = [0] + arr
 answer = float('inf')
 j = 0 
+sum_val = 0
 
-for i in range(n):
-
-    temp = arr[i]
-    while j < n and temp < s:
-        temp += arr[j]
+for i in range(1, n+1):
+    #print(sum_val, end =" ")
+    #포인트를 움직이는 조건 1. s에 미만 일때 움직여
+    while j < n and sum_val < s:
+        sum_val += arr[j]
         j += 1
-
-    answer= min(j-i+1, answer)
-
+    if sum_val >= s: 
+        answer= min(j-i, answer)
+    sum_val -= arr[i]
 print(answer)    
 
 # Please write your code here.
