f9fc1af3f09f1d7a066cbdd44cf0c28e46032b59
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 10:38:48 +0900

    Title: 점 개수 세기, Time: 90ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 250320/점 개수 세기/count-number-of-points.py
+++ 250320/점 개수 세기/count-number-of-points.py
@@ -17,16 +17,29 @@ for i in new:
 
 
 def find_min(x):
-    for i in range(len(new)):
-        if x <= new[i]:
-            return i
-    return -1
+    left = 0
+    right = len(new) - 1
+    while left < right:
+        mid = (left+right)//2
+        if x <= new[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1 
+    
+    return left
 
 def find_max(x):
-    for i in range(len(new)-1, -1, -1):
-        if new[i] <= x :
-            return i         
-    return -1
+    left = 0
+    right = len(new)- 1
+    while left < right:
+        mid = (left+right)//2
+        if x <= new[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1 
+    
+    return right
+
 
 
 
@@ -34,7 +47,4 @@ def find_max(x):
 for x1,x2 in queries:
     left = find_min(x1)
     right = find_max(x2)
-    if left == -1 or right == -1:
-        print(0) 
-    else:
-        print(right-left + 1)
+    print(right-left + 1)
