1ce5655c01a729683f9dd8311dcb5ddb1f216057
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 12:10:26 +0900

    Title: 마라톤 중간에 택시타기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250320/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.py
@@ -0,0 +1,44 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+L = [0]*(n)
+R = [0]*(n)
+
+L[0] = float('inf')
+R[-1] = float('inf')
+for i in range(1, n):
+    L[i] = min(L[i-1], abs(x[i-1] - x[i]) + abs(y[i-1] - y[i]))
+
+for i in range(n-2, -1, -1):
+    R[i] = min(R[i+1], abs(x[i+1] - x[i]) + abs(y[i+1] - y[i]))
+# Please write your code here.
+
+L[0] = 0
+L[-1] = 0
+R[0] = 0
+R[-1] = 0 
+
+max_idx = 0
+max_value = 0
+for i in range(n):
+    if max_value < L[i]:
+        max_value = L[i]
+        max_idx = i
+    
+    if max_value < R[i]:
+        max_value = R[i]
+        max_idx = i
+
+
+answer = 0
+prev = 0 
+
+for cur in range(1, n):
+    if cur == max_idx:
+        continue
+    answer += abs(x[prev] - x[cur])
+    answer += abs(y[prev] - y[cur])
+    prev = cur
+
+print(answer)
