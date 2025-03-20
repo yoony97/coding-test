fdd667cb724436c1284ae3295cf34865eec8f6f5
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 10:51:11 +0900

    Title: 점 개수 세기 2, Time: 230ms, Memory: 28MB, Status: Runtime Error - CodeTree

---
+++ 250320/점 개수 세기 2/count-number-of-points-2.py
@@ -0,0 +1,35 @@
+from sortedcontainers import SortedSet
+
+n, q = map(int, input().split())
+
+points = [tuple(map(int, input().split())) for _ in range(n)]
+queries = [tuple(map(int, input().split())) for _ in range(q)]
+nums = SortedSet()
+maps = {}
+
+for (x,y) in points:
+    nums.add((x,y))
+
+idx = 0
+for (x,y) in nums:
+    maps[str(x)+str(y)] = idx
+    idx += 1
+
+#print(maps)
+
+for (x1,y1,x2,y2) in queries:
+    left_idx = nums.bisect_left((x1,y1))
+    right_idx = nums.bisect_right((x2,y2))
+    if left_idx == right_idx-1:
+        print(1)
+    else:
+        lx,ly = nums[left_idx]
+        rx,ry = nums[right_idx-1]
+
+        print(maps[str(rx)+str(ry)]- maps[str(lx)+str(ly)])
+        
+    
+
+
+
+    
