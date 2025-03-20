856b9c9eafe90f4c689d6ad3090fee42fa7727a6
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 21:20:30 +0900

    Title: 가장 짧은 부분합, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250320/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,17 @@
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+answer = float('inf')
+
+
+for i in range(n):
+    j = i+1
+    temp = arr[i]
+    while j < n and temp <= s:
+        temp += arr[j]
+        j += 1
+
+    answer= min(j-i+1, answer)
+
+print(answer)    
+
+# Please write your code here.
