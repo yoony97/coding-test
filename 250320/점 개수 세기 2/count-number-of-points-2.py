e0341f8ba2e970acb938458ea59bd057e21ca713
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 10:54:33 +0900

    Title: 점 개수 세기 2, Time: 466ms, Memory: 38MB, Status: Wrong Answer - CodeTree

--- 250320/점 개수 세기 2/count-number-of-points-2.py
+++ 250320/점 개수 세기 2/count-number-of-points-2.py
@@ -20,7 +20,10 @@ for (x,y) in nums:
 for (x1,y1,x2,y2) in queries:
     left_idx = nums.bisect_left((x1,y1))
     right_idx = nums.bisect_right((x2,y2))
-    if left_idx == right_idx-1:
+    if left_idx == len(nums) and right_idx == len(nums):
+        print(0)
+        
+    elif left_idx == right_idx-1:
         print(1)
     else:
         lx,ly = nums[left_idx]
