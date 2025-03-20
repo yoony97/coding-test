96818b00604464cfa5ccf11bda0c0665ac294b3b
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 12:28:02 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250320/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+intervals = [tuple(map(int, input().split())) for _ in range(n)]
+
+points = []
+for s,e in intervals:
+    points.append((s,1))
+    points.append((e,-1))
+
+points.sort()
+
+max_count = 0
+current = 0
+for s, v in points:
+    current += v
+    if max_count > current:
+        max_count = current
+
+print(max_count) 
+
+# Please write your code here.
