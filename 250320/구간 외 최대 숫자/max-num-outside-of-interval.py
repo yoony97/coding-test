46dd7a90791d8163be7dac959b60ee1efec434f7
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 11:22:48 +0900

    Title: 구간 외 최대 숫자, Time: 246ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 250320/구간 외 최대 숫자/max-num-outside-of-interval.py
+++ 250320/구간 외 최대 숫자/max-num-outside-of-interval.py
@@ -2,19 +2,24 @@ n, q = map(int, input().split())
 arr = list(map(int, input().split()))
 queries = [tuple(map(int, input().split())) for _ in range(q)]
 
+L = [0]*(n+1)
+R = [0]*(n+1)
+for i in range(n):
+    L[i+1] = max(L[i], arr[i])
+
+for idx, i in enumerate(arr[::-1]):
+    R[idx+1] = max(R[idx], i)
+
+R = list(reversed(R))
+
+
+
+
+
 
 for a,b in queries:
-    max_value = 0
+
     left = a-2
     right = b
 
-    while left >= 0:
-        max_value = max(arr[left], max_value)
-        left -= 1
-
-    while right <= len(arr)-1:
-        max_value = max(arr[right], max_value)
-        right += 1
-
-
-    print(max_value)
+    print(max(L[left], R[right]))
