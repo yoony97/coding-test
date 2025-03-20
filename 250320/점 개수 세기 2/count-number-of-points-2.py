0fe693c4274a4f27d57fe84f229dd58d794010bf
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-20 10:56:47 +0900

    Title: 점 개수 세기 2, Time: 8029ms, Memory: 79MB, Status: Time Limit Exceed - CodeTree

--- 250320/점 개수 세기 2/count-number-of-points-2.py
+++ 250320/점 개수 세기 2/count-number-of-points-2.py
@@ -4,35 +4,17 @@ n, q = map(int, input().split())
 
 points = [tuple(map(int, input().split())) for _ in range(n)]
 queries = [tuple(map(int, input().split())) for _ in range(q)]
-nums = SortedSet()
-maps = {}
 
-for (x,y) in points:
-    nums.add((x,y))
+# (x, y)를 저장하는 정렬된 Set
+nums = SortedSet(points)
 
-idx = 0
-for (x,y) in nums:
-    maps[str(x)+str(y)] = idx
-    idx += 1
+for (x1, y1, x2, y2) in queries:
+    left_idx = nums.bisect_left((x1, float('-inf')))  
+    right_idx = nums.bisect_right((x2, float('inf')))  
+    count = 0
+    for i in range(left_idx, right_idx):
+        x, y = nums[i]
+        if y1 <= y <= y2:
+            count += 1
 
-#print(maps)
-
-for (x1,y1,x2,y2) in queries:
-    left_idx = nums.bisect_left((x1,y1))
-    right_idx = nums.bisect_right((x2,y2))
-    if left_idx == len(nums) and right_idx == len(nums):
-        print(0)
-        
-    elif left_idx == right_idx-1:
-        print(1)
-    else:
-        lx,ly = nums[left_idx]
-        rx,ry = nums[right_idx-1]
-
-        print(maps[str(rx)+str(ry)]- maps[str(lx)+str(ly)])
-        
-    
-
-
-
-    
+    print(count)
