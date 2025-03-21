b08716ae08ec1fb9dad7de4a71c185c76b2644f2
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-21 12:39:50 +0900

    Title: 자연수 n개의 합, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 250321/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -1,12 +1,20 @@
+def get_sum(n):
+    return n*(n+1)//2
+
 s = int(input())
-psum = [0]
-answer = 0 
-for i in range(1, s):
-    psum.append(psum[i-1] + i)
-    if psum[i] > s:
-        answer = i-1
+left = 0
+right = s
+answer = 0
+while left <= right:
+    mid = (left+right)//2
+    if get_sum(mid)  >  s: ## 1부터 mid까지 더한 값이 s 보다 클경우 right를 줄인다.
+        right = mid -1
+    elif get_sum(mid) < s: 
+        answer = max(answer, mid)
+        left  = mid + 1
+    else:
+        answer = mid
         break
-    if psum[i] == s:
-        answer= i
-        break
+
 print(answer)
+        
