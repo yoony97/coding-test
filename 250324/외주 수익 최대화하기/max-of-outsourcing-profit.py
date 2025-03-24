0536d8416f532e0b8312e39cab7e4e0568f3f023
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-24 11:47:50 +0900

    Title: 외주 수익 최대화하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250324/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,15 @@
+n = int(input())
+job = []
+for i in range(n):
+    t, p = map(int, input().split())
+    job.append((t, p))
+
+dp = [0]*(n+5)
+#dp[i] : i 번째 날에 벌 수 있는 최대 수익
+#dp[i] = max(dp[i-t-1]+p, p)
+for i in range(n-1, -1, -1):
+    t, p = job[i]
+    dp[i]  = max(dp[i+t]+p, dp[i+1])
+    
+
+print(max(dp[:n]))
