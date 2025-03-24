8b24aadedf0ad644704d70fcb283cc5e655b7388
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-24 13:36:59 +0900

    Title: 조삼모사, Time: 1014ms, Memory: 24MB, Status: Passed - CodeTree

--- 250324/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ 250324/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -3,7 +3,7 @@
 # 아침에 n//2 개, 저녁에 n//2개 하면 된다.
 # 1,2 일을 한다 -> 12/21 일을 한다
 
-# def combination(job, ):
+from itertools import combinations
 n = int(input())
 answer = float('inf')
 job_list = [i for i in range(n)]
@@ -11,36 +11,39 @@ arr = []
 for i in range(n):
     arr.append(list(map(int, input().split())))
 
-def combination(job):
-    result = []
-    for i in job:
-        for j in job:
-            if i != j:
-                result.append((i,j))
-    return result
+# def combination(job):
+#     result = []
+#     for i in job:
+#         for j in job:
+#             if i != j:
+#                 result.append((i,j))
+#     return result
 
 
-def back(morning):
+def back(idx, morning):
     global answer, job_list
 
     if len(morning) == n//2:
         mscore = 0
         ascore = 0
         afternoon = list(set(job_list) - set(morning))
-        for x, y in combination(morning):
+        for x, y in combinations(morning,2):
             mscore += arr[x][y]
+            mscore += arr[y][x]
         
-        for x, y in combination(afternoon):
+        for x, y in combinations(afternoon,2):
             ascore += arr[x][y]
+            ascore += arr[y][x]
         
         answer = min(answer, abs(mscore-ascore))
         return
     
-    for i in range(n):
-        if i not in morning:
-            morning.append(i)
-            back(morning)
-            morning.pop()
+    for i in range(idx+1, n):
+        #if i not in morning:
+        morning.append(i)
+        back(i, morning)
+        morning.pop()
 
-back([])
+#print(list(combinations([1,2,3],2)))
+back(0, [])
 print(answer)
