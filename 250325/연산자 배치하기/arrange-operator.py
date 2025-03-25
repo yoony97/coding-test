ce418b436741202c61b1e87e033dba027e3a56bb
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-25 13:29:13 +0900

    Title: 연산자 배치하기, Time: 58ms, Memory: 17MB, Status: Passed - CodeTree

--- 250325/연산자 배치하기/arrange-operator.py
+++ 250325/연산자 배치하기/arrange-operator.py
@@ -10,31 +10,22 @@ n = int(input())
 numbers = list(map(int, input().split()))
 count = list(map(int, input().split())) #[덧셈, 뺄샘, 곱셈] 개수
 
-def back(cur_num, count, value, start):
-    global maximum, minimum
-    if cur_num == n: #연산을 n번 했다.
-        minimum = min(value, minimum)
-        maximum = max(value, maximum)
-        return 
-    
-    for i in range(start+1, len(numbers)):
-        if count[0] > 0:
-            count[0]-=1
-            back(cur_num+1, count, value + numbers[i], i)  
-            count[0] += 1        
+def back(idx, value, plus, minus, mul):
+    global minimum, maximum
+    if idx == n:
+        minimum = min(minimum, value)
+        maximum = max(maximum, value)
+        return
 
-        if count[1] > 0:
-            count[1]-=1
-            back(cur_num+1, count, value - numbers[i], i)      
-            count[1] += 1 
+    if plus:
+        back(idx + 1, value + numbers[idx], plus - 1, minus, mul)
+    if minus:
+        back(idx + 1, value - numbers[idx], plus, minus - 1, mul)
+    if mul:
+        back(idx + 1, value * numbers[idx], plus, minus, mul - 1)
+
+# 시작은 numbers[0], idx = 1부터 시작
+back(1, numbers[0], count[0], count[1], count[2])
         
-        if count[2] > 0:
-            count[2]-=1
-            back(cur_num+1, count, value * numbers[i], i)      
-            count[2]+= 1        
-        
-
-
-back(1, count, numbers[0], 0)
 
 print(minimum, maximum)
