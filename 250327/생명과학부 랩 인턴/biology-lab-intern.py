3c6ad91535dca171e7c90c43f7350f614990dcad
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-27 16:18:55 +0900

    Title: 생명과학부 랩 인턴, Time: 994ms, Memory: 59MB, Status: Passed - Codetree

---
+++ 250327/생명과학부 랩 인턴/biology-lab-intern.py
@@ -0,0 +1,71 @@
+"""
+  시작 시간: 15:26
+- 곰팡이 크기, 속력
+i =  0
+1. 곰팡이 채취
+    - i 열부터 위에서 아래로 탐색
+    - 곰팡이 발견시 - 채취(빈칸이 됨)
+2. 곰팡이 이동 (벽에 부딪히면 방향 바꿈/시간 소요X)
+3. 한칸에 곰팡이가 두마리 이상이면 크기가 큰 곰팡이가 다른 큰 곰팡이를 모두 먹음
+i+= 1 
+"""
+from collections import deque
+
+n, m, k = map(int, input().split())
+#arr = [[deque([])]*m for _ in range(n)] #얕은 복사가 됨
+arr = [[deque() for _ in range(m)] for _ in range(n)]
+dx = [-1, 1, 0, 0] #위, #아래, 오른쪽, 왼쪽
+dy = [0, 0, 1, -1]
+change_d = {0:1, 1:0, 2:3, 3:2}
+
+for _ in range(k):
+    x, y, v, d, size = map(int, input().split())
+    arr[x-1][y-1].append((v,d-1,size))
+
+
+def move_target():
+    new_arr = [[deque() for _ in range(m)] for _ in range(n)]
+
+    for x in range(n):
+        for y in range(m):
+            while arr[x][y]:  # ← 한 칸에 여러 개 있을 수 있으니까 while로!
+                v, d, size = arr[x][y].popleft()
+                cx, cy = x, y
+                for _ in range(v):
+                    nx = cx + dx[d]
+                    ny = cy + dy[d]
+                    if not (0 <= nx < n and 0 <= ny < m):
+                        d = change_d[d]
+                        nx = cx + dx[d]
+                        ny = cy + dy[d]
+                    cx, cy = nx, ny
+                new_arr[cx][cy].append((v, d, size))
+    
+    return new_arr
+
+def eat():
+    for x in range(n):
+        for y in range(m):
+            if len(arr[x][y]) >= 2:
+                target = (-1, -1, -1)
+                while arr[x][y]:
+                    v, d, size  = arr[x][y].popleft()
+                    if target[2] < size:
+                        target = (v, d, size)
+                #다 제거 하고 타겟만 넣음
+                arr[x][y].append(target)
+
+
+answer = 0
+
+for y in range(m):
+    for x in range(n):
+        if arr[x][y]: #만약 곰팡이가 있다면 제거
+            v, d, size = arr[x][y].popleft()
+            answer += size
+            break
+    arr = move_target()
+    eat()
+    
+print(answer)
+#print(arr)
