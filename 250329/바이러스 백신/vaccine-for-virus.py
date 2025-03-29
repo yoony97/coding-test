5450daee8e080a4d1a731de4fe949bd50e92d551
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-29 13:22:38 +0900

    Title: 바이러스 백신, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250329/바이러스 백신/vaccine-for-virus.py
+++ 250329/바이러스 백신/vaccine-for-virus.py
@@ -4,60 +4,11 @@ from collections import deque
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def simulate(hist):
-    doctors = deque([h for h in hist])
-    new_arr = [[0]*N for _ in range(N)]
-    for i in hist:
-        x, y, _ = i
-        new_arr[x][y] = 2
-    
-    for i in range(N): 
-        for j in range(N):
-            if arr[i][j] == 1:
-                new_arr[i][j] = 1
-    
-    visited = [[False]*N for _ in range(N)]
-    max_time = 0
-    while doctors:
-        x, y, t = doctors.popleft()
-        visited[x][y] = True
-        if arr[x][y] == 0:
-            max_time = max(max_time, t)
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                if new_arr[nx][ny] == 0:
-                    doctors.append((nx,ny, t+1))
-                    new_arr[nx][ny] = -1
-                
-    #다 진행 후에, 0이 남아있나 확인해야함
-    for i in range(N):
-        for j in range(N):
-            if new_arr[i][j] == 0:
-                #불가능 하다.
-                return float('inf')
-    return max_time
-    
-answer = float('inf')
-def btk(cur_num, start, hist):
-    global answer
-    if cur_num == M:
-        max_time = simulate(hist)
-        answer= min(max_time, answer)
-        return
-
-    for i in range(start, len(total)):
-        hist.append(total[i])
-        btk(cur_num+1, i+1, hist)
-        hist.pop()
-
 N, M = map(int, input().split())
 arr = [ ]
 total = []
 isexist = False
-
+answer = float('inf')
 
 for i in range(N):
     row = list(map(int, input().split()))
@@ -69,12 +20,54 @@ for i in range(N):
     arr.append(row)
 
 
+def simulate(hist):
+    doctor = deque([i for i in hist])
+    new_arr = [[arr[i][j] for j in range(N)] for i in range(N)]
+    visited = [[False]*N for _ in range(N)]
+    max_time = 0
 
-if isexist:
-    btk(0, 0, [])
-    if answer == float('inf'):
-        answer = -1
+    while doctor:
+        x, y, t = doctor.popleft()
+        visited[x][y] = True
+        if arr[x][y] == 0: #바이러스 칸에서 정화 했을 경우
+            max_time = max(max_time, t)
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                if new_arr[nx][ny] == 0:
+                    doctor.append((nx, ny, t+1))
+                    new_arr[nx][ny] = -1
+                elif new_arr[nx][ny] == 2:
+                    doctor.append((nx, ny, t))
+                
+    
 
-    print(answer)
-else:
-    print(0)
+    for i in range(N):
+        for j in range(N):
+            if new_arr[i][j] == 0:
+                return float('inf')
+    return max_time
+
+
+def btk(start, hist):
+    global answer 
+    if len(hist) == M:
+        max_time = simulate(hist)    
+        answer = min(answer, max_time)
+        return
+    
+    for i in range(start, len(total)):
+        hist.append(total[i])
+        btk(i+1, hist)
+        hist.pop()
+
+btk(0, [])
+if answer == float('inf'):
+    answer = -1
+
+print(answer)
+                
+
+    
