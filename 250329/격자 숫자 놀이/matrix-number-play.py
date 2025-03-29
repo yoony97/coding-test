2d6ed53cacdbdd698c12a06d370df5141fd546dc
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-29 12:21:00 +0900

    Title: 격자 숫자 놀이, Time: 228ms, Memory: 23MB, Status: Passed - Codetree

--- 250329/격자 숫자 놀이/matrix-number-play.py
+++ 250329/격자 숫자 놀이/matrix-number-play.py
@@ -1,84 +1,106 @@
-#행의 개수 >= 열의 개수
-  # 행에 대해서 정렬(빈도수가 적은 순서대로)
-  # 횟수가 같을 경우, 작은 순
-  # 정렬 수행시, 숫자와 빈도수 출력
-# 행의 개수 < 열의 개수
-  # 열에 대해서 정렬
+MAX_N = 100
+MAX_NUM = 100
 
-# 행 길이 또는 열길이 >= 100 처음 100개 만 받음
+# 변수 선언 및 입력
+n, m = 3, 3
+r, c, k = tuple(map(int, input().split()))
 
-arr = []
-r, c, k = map(int, input().split())
-for i in range(3):
-    row = list(map(int, input().split()))
-    arr.append(row)
+grid = [
+    [0 for _ in range(2 * MAX_N + 1)]
+    for _ in range(2 * MAX_N + 1)
+]
 
 
-def count(row):
-    new_row = []
-    result = []
-    for i in list(set(row)):
-        if i != 0:        
-            b = row.count(i)
-            new_row.append((b,i))
-
-    new_row.sort(key = lambda x: (x[0], x[1]))
-
-    for i in new_row:
-        result.extend([i[1], i[0]])
-
-    return result, len(result)
-
-def transpose(arr):
-    #list(*zip(i) for i in arr:
-    return [ list(i)  for i in list(zip(*arr))]
-
-
-def sorting(arr):
-    new_arr = []
-    temp = []
-    max_len = 0 
-    for i in arr[:100]: #100까지 제한
-        i = i[:100] #제한
-        new_row, length = count(i)
-        max_len = max(max_len, length)
-        temp.append(new_row)
+# row 행에 대해 숫자 놀이를 진행합니다.
+def row_play(row):
+    # 각 숫자에 대해 빈도수를 구해줍니다.
+    # 정렬시 빈도수, 숫자 순으로 오름차순 정렬이 되도록
+    # (빈도수, 숫자) 형태로 저장해줍니다.
+    pairs = list()
+    for num in range(1, MAX_NUM + 1):
+        frequency = sum([
+            1
+            for col in range(1, m + 1)
+            if grid[row][col] == num
+        ])
+        
+        if frequency:
+            pairs.append((frequency, num))
     
-    for t in temp:
-        new_row = t 
-        if len(t) <= max_len:
-            new_row.extend([0]*(max_len - len(t)))
-        new_arr.append(new_row)
-    return new_arr
+    # 숫자를 새로 적어줘야 하므로,
+    # 그 전에 기존 row 행에 있던 숫자들을 전부 0으로 바꿔줍니다.
+    for col in range(1, m + 1):
+        grid[row][col] = 0
+    
+    pairs.sort()
+    
+    # row 행에 새로운 숫자를 차례대로 적어줍니다.
+    for i, (frequency, num) in enumerate(pairs):
+        grid[row][i * 2 + 1] = num
+        grid[row][i * 2 + 2] = frequency
 
-def simulation(arr):
-    max_len = 0
-    if len(arr) < len(arr[0]): #행의 크기가 열의 크기보다 크거나 같을 떄,
-        arr = transpose(arr)
-        arr = sorting(arr)
-        arr = transpose(arr)
+    return len(pairs) * 2
+
+
+# col 열에 대해 숫자 놀이를 진행합니다.
+def col_play(col):
+    # 각 숫자에 대해 빈도수를 구해줍니다.
+    # 정렬시 빈도수, 숫자 순으로 오름차순 정렬이 되도록
+    # (빈도수, 숫자) 형태로 저장해줍니다.
+    pairs = list()
+    for num in range(1, MAX_NUM + 1):
+        frequency = sum([
+            1
+            for row in range(1, n + 1)
+            if grid[row][col] == num
+        ])
+        
+        if frequency:
+            pairs.append((frequency, num))
+    
+    # 숫자를 새로 적어줘야 하므로,
+    # 그 전에 기존 col 열에 있던 숫자들을 전부 0으로 바꿔줍니다.
+    for row in range(1, n + 1):
+        grid[row][col] = 0
+    
+    pairs.sort()
+    
+    # col 열에 새로운 숫자를 차례대로 적어줍니다.
+    for i, (frequency, num) in enumerate(pairs):
+        grid[i * 2 + 1][col] = num
+        grid[i * 2 + 2][col] = frequency
+
+    return len(pairs) * 2
+
+
+def simulate():
+    global n, m
+    # 행의 개수가 열의 개수와 일치하거나 더 많다면
+    # 행 단위로 진행 후, 최대로 긴 열의 크기를 구합니다.
+    if n >= m:
+        m = max([
+            row_play(row) for row in range(1, n + 1)
+        ])
+    # 열의 개수가 더 많다면
+    # 열 단위로 진행 후, 최대로 긴 행의 크기를 구합니다.
     else:
-        arr= sorting(arr)
+        n = max([
+            col_play(col) for col in range(1, m + 1)
+        ])
 
-    return arr
 
-def solve(arr):
-    answer = 0
-    if 0 <= r-1 < len(arr) and 0 <= c-1 < len(arr[0]):
-        if arr[r-1][c-1] == k:
-            return answer
-            
-    while True:
-        answer += 1
-        if answer >= 100:
-            return -1
+for i in range(1, n + 1):
+    given_row = list(map(int, input().split()))
+    for j, elem in enumerate(given_row, start=1):
+        grid[i][j] = elem
 
-        arr = simulation(arr)
-        if 0 <= r-1 < len(arr) and 0 <= c-1 < len(arr[0]):
-            if arr[r-1][c-1] == k:
-                return answer
-
+ans = -1
+# 최대 100초 동안 시뮬레이션을 진행합니다.
+for t in range(0, 100):
+    if grid[r][c] == k:
+        ans = t
+        break
     
+    simulate()
 
-
-print(solve(arr))
+print(ans)
