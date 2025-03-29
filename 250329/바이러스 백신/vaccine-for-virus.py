6395f3d4c4af060d3a9b62f576ef3b937d9b6386
yoony97 <88752851+yoony97@users.noreply.github.com>
2025-03-29 13:06:50 +0900

    Title: 바이러스 백신, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250329/바이러스 백신/vaccine-for-virus.py
+++ 250329/바이러스 백신/vaccine-for-virus.py
@@ -15,24 +15,23 @@ def simulate(hist):
         for j in range(N):
             if arr[i][j] == 1:
                 new_arr[i][j] = 1
-
     
     visited = [[False]*N for _ in range(N)]
     max_time = 0
     while doctors:
         x, y, t = doctors.popleft()
         visited[x][y] = True
-        max_time = max(max_time, t)
+        if arr[x][y] == 0:
+            max_time = max(max_time, t)
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-                
                 if new_arr[nx][ny] == 0:
                     doctors.append((nx,ny, t+1))
                     new_arr[nx][ny] = -1
                 
-    
     #다 진행 후에, 0이 남아있나 확인해야함
     for i in range(N):
         for j in range(N):
@@ -46,7 +45,6 @@ def btk(cur_num, start, hist):
     global answer
     if cur_num == M:
         max_time = simulate(hist)
-        print(hist, max_time)
         answer= min(max_time, answer)
         return
 
@@ -58,16 +56,25 @@ def btk(cur_num, start, hist):
 N, M = map(int, input().split())
 arr = [ ]
 total = []
+isexist = False
+
 
 for i in range(N):
     row = list(map(int, input().split()))
     for j in range(N):
+        if row[j] == 0:
+            isexist = True
         if row[j] == 2:
             total.append((i,j,0))
     arr.append(row)
 
-btk(0, 0, [])
-if answer == float('inf'):
-    answer = -1
 
-print(answer)
+
+if isexist:
+    btk(0, 0, [])
+    if answer == float('inf'):
+        answer = -1
+
+    print(answer)
+else:
+    print(0)
